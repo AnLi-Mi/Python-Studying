6015160cc07b481ada3abe7e1f29f263a4aa2a55
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-04 15:18:00 +0200

    testing the simplext encyption with .replace()

--- Project Crypto.py
+++ Project Crypto.py
@@ -3,23 +3,35 @@ print('----------------------Project-Crypto---------------------------')
 
 import random, string
 
-en = input("Type 'True' if you wnat to encypt")
-de = input("Type 'True' if you wnat to decypt")
-word = input ("Type in the word")
-word_en = []
-word_de = []
+task = input("Type 'en' if you wnat to encrypt or 'de' if you want to decrypt")
+word = str(input("Type in the word"))
+#word_en = []
+#word_de = []
 
-if en==true:
-    for sign in word:
+#if en==true:
+ #   for sign in word:
         #sposob enkrypcji
-        word_en.append(sign)
-    print(''.join.word_en())
-else:
-    for sign in word:
+  #      word_en.append(sign)
+   # print(''.join.word_en())
+#else:
+ #   for sign in word:
         #odwrocony sposob enkrypcji
-        word_en.append(sign)
-    print(''.join.word_de())
+  #      word_en.append(sign)
+   # print(''.join.word_de())
 
-        
+print('-------------------------------------------------')
 
+       
+if task=="en":
+    #word.replace("a", "b")
+    #word.replace("c", "d")
+    #word.replace("e", "f")
+    print(word.replace("a", "b"))
+elif task=="de":
+    #word.replace("b", "a")
+    #word.replace("d", "c")
+    #word.replace("f", "e")
+    print(word.replace("b", "a"))
+else:
+    print('error')
 
