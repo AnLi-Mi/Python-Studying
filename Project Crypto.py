53f945c06314a3f65aa79ae124875c8aec12ea07
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-04 14:57:44 +0200

    my first draft

--- Project Crypto.py
+++ Project Crypto.py
@@ -1,1 +1,25 @@
-print('Project -  Crypto')
+print('----------------------Project-Crypto---------------------------')
+
+
+import random, string
+
+en = input("Type 'True' if you wnat to encypt")
+de = input("Type 'True' if you wnat to decypt")
+word = input ("Type in the word")
+word_en = []
+word_de = []
+
+if en==true:
+    for sign in word:
+        #sposob enkrypcji
+        word_en.append(sign)
+    print(''.join.word_en())
+else:
+    for sign in word:
+        #odwrocony sposob enkrypcji
+        word_en.append(sign)
+    print(''.join.word_de())
+
+        
+
+
