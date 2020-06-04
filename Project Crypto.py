f3323741be02769d17ac5eadbe6bd95bc2f3c849
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-04 16:36:40 +0200

    clearing my code, add comprehension version instuceted by teacher

--- Project Crypto.py
+++ Project Crypto.py
@@ -3,12 +3,12 @@ print('----------------------Project-Crypto---------------------------')
 
 import random, string
 
+
+
 task = input("Type 'E' if you wnat to encrypt or 'D' if you want to decrypt")
 word = str(input("Type in the secret word: "))
-word_en = []
-word_de = []
-
-
+word_new = []
+    
 keys= string.ascii_lowercase
 values = keys[::-1]
 
@@ -17,19 +17,27 @@ code_en=dict(zip(keys, values))
 code_de=dict(zip(values, keys))
 
 if task=="e" or task=="E":
-   for letter in word:
-       letter = code_en[letter]
-       word_en.append(letter)
+    for letter in word:
+        letter = code_en[letter]
+        word_new.append(letter)
+                    
+elif task=="d" or task=="D":
+    for letter in word:
+        letter = code_de[letter]
+        word_new.append(letter) 
+    
+print("".join(word_new))
+
+print('----------------------with compehension---------------------------')
+
+if task=="e" or task=="E":
+   word_en = [code_en[letter] for letter in word]
    print("Encrypted word is: " + "".join(word_en))    
             
 elif task=="d" or task=="D":
-    for letter in word:
-       letter = code_de[letter]
-       word_de.append(letter)
-    print("Decrypted word is: " + "".join(word_de))
+    word_de = [code_de[letter] for letter in word]
+    print("Encrypted word is: " + "".join(word_de))
 else:
     print('error')
-
-
 
 
