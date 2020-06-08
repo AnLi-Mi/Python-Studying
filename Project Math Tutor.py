7a6b6b45a851bf9b71b049139619050840931e8a
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-08 18:31:49 +0200

    dodanie komunikatu  wynikiem prawidlowych odpowiedzi

--- Project Math Tutor.py
+++ Project Math Tutor.py
@@ -2,17 +2,20 @@ import random
 
 x = int(input("Choose number of prectice questions"))
 i=1
+r=0
 
 while i < x+1:
-    a = random.randint(1, 101)
-    b = random.randint(1, 101)
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
     print(f"{a} x {b} = ")
     ans = int(input("wynik: "))
     if ans == a*b:
-        print("Dobrze")
-    else:
-        print("Zle")
-
+        r+=1
     i+=1
+
+result=round(r/x*100, 2)
+print ('Thank you for playing!')
+print (f'You hve managed to have {r} correct answers')
+print (f'Your test result is {result}%')
     
         
