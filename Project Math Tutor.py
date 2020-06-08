9cf4e7b378410da1337583bd12b4082ac573e6d3
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-08 19:30:11 +0200

    dodanie timera i komunikatu z czasem roziwazywania zadan

--- Project Math Tutor.py
+++ Project Math Tutor.py
@@ -1,21 +1,30 @@
 import random
+from time import time as t
+
 
 x = int(input("Choose number of prectice questions"))
+od = int(input("Choose the begging of the number range"))
+do = int(input("Choose the end of the number range"))
 i=1
 r=0
 
+start = t()
+
 while i < x+1:
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
+    a = random.randint(od, do)
+    b = random.randint(od, do)
+    prod = a*b
     print(f"{a} x {b} = ")
     ans = int(input("wynik: "))
-    if ans == a*b:
+    if ans == prod:
         r+=1
     i+=1
-
+end=t()
 result=round(r/x*100, 2)
 print ('Thank you for playing!')
-print (f'You hve managed to have {r} correct answers')
+print (f'You have managed to have {r} correct answers')
 print (f'Your test result is {result}%')
+print (f'It took you {round(end-start,1)} seconds, which is {round((end-start)/x,1)} seconds per question.')
+
     
         
