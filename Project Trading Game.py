5e931ffaf0f4743288cc40479d067303be6a26d7
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-09 15:07:54 +0200

    added betting option

--- Project Trading Game.py
+++ Project Trading Game.py
@@ -4,9 +4,19 @@ import random
 
 bag=['green', 'green', 'green', 'green', 'green', 'green', 'red', 'red', 'red', 'red']
 wallet = 1000
-los = random.choice(bag)
-print(los)
-if los=='green':
-    print('You won!')
-else:
-    print('You lost!')
+games = int(input('how many times do you want to play?: '))
+
+for game in range (1,games+1):                 
+    bet = int(input('How much do you bet?: '))
+    los = random.choice(bag)
+    print(los)
+    if los=='green':
+        print('You won!')
+        wallet = wallet + bet
+        print (f'You have {wallet}$') 
+    else:
+        print('You lost!')
+        wallet = wallet - bet
+        print (f'You have {wallet}$')
+
+
