e5ba128b6679d62f05be4c4779a475947e990faa
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-09 17:24:51 +0200

    indicating multiplers

--- Project Euler 4.py
+++ Project Euler 4.py
@@ -1,18 +1,23 @@
 # project Euler 4 - largest palindrome of two 3 - digit numbers
 
 palindromes=[]
+multiplers = {}
 for num1 in range (100,1000):
     for num2 in range (100,1000):
         product = str(num1*num2)
         if len(product)==5:
             if product[0] == product[-1] and product[1] == product[-2] :
+                multiplers[product] = (num1, num2)
                 product=int(product)
                 palindromes.append(product)
                 #print(f'{num1} x {num2} = {product}')
         else:
             if product[0] == product[-1] and product[1] == product[-2] and product[2]==product[-3]:
+                multiplers[product] = (num1, num2)
                 product=int(product)
                 palindromes.append(product)
                 #print(f'{num1} x {num2} = {product}')
 
-print(max(palindromes))
+answer = max(palindromes)
+print(answer)
+print(multiplers['answer'])
