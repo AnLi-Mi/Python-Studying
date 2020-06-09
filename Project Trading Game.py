e06b0409c889572840ed7736846c15ea21958803
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-09 15:26:07 +0200

    adding limit on amout of money

--- Project Trading Game.py
+++ Project Trading Game.py
@@ -6,17 +6,21 @@ bag=['green', 'green', 'green', 'green', 'green', 'green', 'red', 'red', 'red', 
 wallet = 1000
 games = int(input('how many times do you want to play?: '))
 
-for game in range (1,games+1):                 
+
+for game in range (1, games+1):                 
     bet = int(input('How much do you bet?: '))
     los = random.choice(bag)
     print(los)
     if los=='green':
-        print('You won!')
         wallet = wallet + bet
-        print (f'You have {wallet}$') 
+        print (f'Great! You have {wallet}$') 
     else:
-        print('You lost!')
         wallet = wallet - bet
-        print (f'You have {wallet}$')
+        print (f'Oh, no... You have {wallet}$')
+    if wallet <500:
+        print ('You lost... You have less than 500$, you can not play')
+        break
+        
+   # playing = str(input('Do you still want to play?; '))
 
 
