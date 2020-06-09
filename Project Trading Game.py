8fc7006f230f9fe0cbf818d437dd436604ecf634
AnLi-Mi <a.lipiarz@gmail.com>
2020-06-09 16:12:45 +0200

    added more marbles

--- Project Trading Game.py
+++ Project Trading Game.py
@@ -2,25 +2,29 @@ print('Project - Trading game simulation')
 
 import random
 
-bag=['green', 'green', 'green', 'green', 'green', 'green', 'red', 'red', 'red', 'red']
+bag=['black', ' green', 'green', 'green', 'green', 'green', 'white', 'red', 'red', 'red']
 wallet = 1000
-games = int(input('how many times do you want to play?: '))
-
-
+games = 3
 for game in range (1, games+1):                 
     bet = int(input('How much do you bet?: '))
     los = random.choice(bag)
-    print(los)
+    print(f'Round {game}: {los}')
     if los=='green':
         wallet = wallet + bet
-        print (f'Great! You have {wallet}$') 
+        print (f'Great! You have {wallet}$')
+    elif los =='black':
+        wallet = wallet + 10*bet
+        print (f'What a luck! You win {10*bet}$. You have {wallet}$')
+    elif los == 'white':
+        wallet = wallet - 5*bet
+        print (f'What a bad luck! You loose {5*bet}$. You have {wallet}$')
     else:
         wallet = wallet - bet
         print (f'Oh, no... You have {wallet}$')
     if wallet <500:
         print ('You lost... You have less than 500$, you can not play')
         break
-        
-   # playing = str(input('Do you still want to play?; '))
+
+
 
 
