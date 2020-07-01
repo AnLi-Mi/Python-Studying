d8b461f0e550c6627a95085ce76724b052e42693
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-01 18:45:03 +0200

    getting a number from a user and printing out that many copies of the message

--- Project Counting Years.py
+++ Project Counting Years.py
@@ -4,7 +4,8 @@
 
 #Extras:
 
-#Add on to the previous program by asking the user for another number and printing out that many copies of the previous message.
+#Add on to the previous program by asking the user for another number
+#and printing out that many copies of the previous message.
 #Hint: order of operations exists in Python
 #Print out that many copies of the previous message on separate lines. 
 #Hint: the string "\n is the same as pressing the ENTER button
@@ -21,5 +22,17 @@ current_yr=date.today().year
 
 name = str(input(f'Please enter your name: '))
 age = int(input(f'Please enter your age: '))
+num = int(input(f'Choose random number between 1 and 10:  '))
 
-print (f'Hi {name}! You will turn 100 in {100-age} years. It will be year {current_yr + 100 - age}.' )
+
+if num not in range (1,10) :
+    num = (int(input(f'Wrong number, please choose one between 1 and 10: ')))
+    
+if num in range (1,10):
+    for i in range (num):
+        print (f'Hi {name}! You will turn 100 in {100-age} years. It will be year {current_yr + 100 - age}.' )
+
+
+
+
+   
