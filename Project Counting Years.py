e2423e2fa7418082ca5ae78672dda92d4cf30db1
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-01 17:53:55 +0200

    importing datatime, preparing inputs for name and age, final message to the user

---
+++ Project Counting Years.py
@@ -0,0 +1,25 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them
+# the year that they will turn 100 years old.
+
+#Extras:
+
+#Add on to the previous program by asking the user for another number and printing out that many copies of the previous message.
+#Hint: order of operations exists in Python
+#Print out that many copies of the previous message on separate lines. 
+#Hint: the string "\n is the same as pressing the ENTER button
+
+#------------------------------solution----------------------------
+
+# importing datetime module to automatate the proces
+
+from datetime import date
+
+current_yr=date.today().year
+
+# defining name and age as an intput
+
+name = str(input(f'Please enter your name: '))
+age = int(input(f'Please enter your age: '))
+
+print (f'Hi {name}! You will turn 100 in {100-age} years. It will be year {current_yr + 100 - age}.' )
