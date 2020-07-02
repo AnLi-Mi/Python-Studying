3b78819e22b0b89ed58ffbe5c5bc5668f804ea2e
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-02 14:07:52 +0200

    i've added an option for the user to choose if thay want to continue paly after each round

--- Project Rock-Paper-Scisors.py
+++ Project Rock-Paper-Scisors.py
@@ -5,16 +5,37 @@ import random
 choose = ['nożyce', 'papier', 'kamień']
 
 def gra(x, y):
-
-    if x == y:
+    score = 0
+    if x.lower() == y:
         print(f'Remis! ')
-    elif (x == "papier" and y== "kamień") or (x == "kamień" and y== "nożyce") or (x == "nożyce" and y== "papier")  :
+    elif (x.lower() == "papier" and y== "kamień") or (x.lower() == "kamień" and y== "nożyce") or (x.lower() == "nożyce" and y== "papier")  :
         print(f'Wygrywa: {x}! ')
+        score=score+1
+    
     else:
         print(f'Wygrywa: {y}!')
-
+        
+    again=str(input(f'Wpisz "Tak" jeśłi chcesz zagrać ponownie: '))
     
+    if again.lower() == "tak":
+        #wybór gracz
+        player1=str(input(f'Wybirz "papier", "nożyce" czy "kamień": '))
 
+        #requesting the input until it's correct
+        while player1 not in ['nożyce', 'papier', 'kamień']:
+            player1=str(input(f'Nieprawidłowy wybór. Wybirz "papier", "nożyce" czy "kamień": '))
+        
+
+        #losowy wybór komputera:
+        player2=random.choice(choose)
+        print(f'Komputer wybrał: {player2}')
+
+        #ponowna gra
+        gra(player1, player2)
+        
+    else:
+        print(f'Dziękuejmy za grę! Zdobyłeś {score} puntów')
+    
 
 #choice of player #1
 
@@ -32,8 +53,10 @@ print(f'Komputer wybrał: {player2}')
 
 #choice of player #2
 
-#player2=str(input(f'Wybirz "papier", "nożyce" czy "kamień": '))
+gra(player1,player2)
 
-gra(player1, player2)
+
+
+
 
 
