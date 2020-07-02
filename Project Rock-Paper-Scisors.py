8524a9f0f8cff9ca2c7baf116d502141a2e26aaa
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-02 14:47:59 +0200

    added score keeping feature

--- Project Rock-Paper-Scisors.py
+++ Project Rock-Paper-Scisors.py
@@ -2,59 +2,39 @@
 
 import random
 
-choose = ['nożyce', 'papier', 'kamień']
+score = 0
+game='tak'
+games=0
 
-def gra(x, y):
-    score = 0
-    if x.lower() == y:
+while game.lower()=='tak':
+
+    #choice of player #1
+    player1=str(input(f'Wybirz "papier", "nożyce" czy "kamień": '))
+
+    #requesting the input until it's correct
+    while player1.lower() not in ['nożyce', 'papier', 'kamień']:
+        player1=str(input(f'Nieprawidłowy wybór. Wybirz "papier", "nożyce" czy "kamień": '))
+        
+    #losowy wybór komputera:
+    choose = ['nożyce', 'papier', 'kamień']
+    player2=random.choice(choose)
+    print(f'Komputer wybrał: {player2}')
+
+    #zasady gry
+    if player1.lower() == player2:
         print(f'Remis! ')
-    elif (x.lower() == "papier" and y== "kamień") or (x.lower() == "kamień" and y== "nożyce") or (x.lower() == "nożyce" and y== "papier")  :
-        print(f'Wygrywa: {x}! ')
+    elif (player1.lower() == "papier" and player2== "kamień") or (player1.lower() == "kamień" and player2== "nożyce") or (player1.lower() == "nożyce" and player2== "papier")  :
+        print(f'Wygrywa: {player1}! ')
         score=score+1
-    
     else:
-        print(f'Wygrywa: {y}!')
+        print(f'Wygrywa: {player2}!')
+    games+=1
+            
+    game=str(input(f'Wpisz "tak" jeśłi chcesz zagrać ponownie: '))
         
-    again=str(input(f'Wpisz "Tak" jeśłi chcesz zagrać ponownie: '))
-    
-    if again.lower() == "tak":
-        #wybór gracz
-        player1=str(input(f'Wybirz "papier", "nożyce" czy "kamień": '))
-
-        #requesting the input until it's correct
-        while player1 not in ['nożyce', 'papier', 'kamień']:
-            player1=str(input(f'Nieprawidłowy wybór. Wybirz "papier", "nożyce" czy "kamień": '))
-        
-
-        #losowy wybór komputera:
-        player2=random.choice(choose)
-        print(f'Komputer wybrał: {player2}')
-
-        #ponowna gra
-        gra(player1, player2)
-        
-    else:
-        print(f'Dziękuejmy za grę! Zdobyłeś {score} puntów')
     
 
-#choice of player #1
-
-player1=str(input(f'Wybirz "papier", "nożyce" czy "kamień": '))
-
-#requesting the input until it's correct
-
-while player1 not in ['nożyce', 'papier', 'kamień']:
-    player1=str(input(f'Nieprawidłowy wybór. Wybirz "papier", "nożyce" czy "kamień": '))
-    
-
-#losowy wybór komputera:
-player2=random.choice(choose)
-print(f'Komputer wybrał: {player2}')
-
-#choice of player #2
-
-gra(player1,player2)
-
+print(f'Dziękuejmy za grę! Zdobyłeś {score}/{games} puntów')
 
 
 
