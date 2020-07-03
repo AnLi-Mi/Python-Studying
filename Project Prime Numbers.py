5f32aeda69d2def3008b37df46056f5083a2c718
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-03 14:28:23 +0200

    function dectecting if num is prime

---
+++ Project Prime Numbers.py
@@ -0,0 +1,18 @@
+#Ask the user for a number and determine whether the number is prime or not.
+#Take this opportunity to practice using functions, described below.
+
+# --------------------------SOLUTION-------------------------------
+
+def prime(x):
+    div=[]
+    for i in range(1, x+1):
+        if x%i==0:
+            div.append(i)
+    if len(div)==2 or len(div)==2:
+        print (f'{x} jest liczbą pierwszą')
+    else:
+        print (f'{x} nie jest liczbą pierwszą')
+    return x, div
+
+num=int(input("Proszę podać liczbę by sprawdzić czy jest ona liczbą pierwszą: "))
+prime(num)
