02dfe1ad342264e843dd022af3c6f252cc67e6cd
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-04 16:55:37 +0200

    all code at once

---
+++ Project Selecting Even Elements.py
@@ -0,0 +1,23 @@
+#Let’s say I give you a list saved in a variable:
+#a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+#Write one line of Python that takes this list a
+#and makes a new list that has only the even elements of this list in it.
+
+# -------------------------SOLUTION----------------------
+
+#creating a function that generats new list out off even elements of given list
+
+def even_list(x):
+    #preparing an empty list to populate it with even elements form list x
+    b = []
+    #going through all elements of the list x and checking if they are dividable by 2
+    for i in range (0, len(x)):
+        if x[i]%2==0:
+            # populating the list b with even elelemnts of x
+            b.append(x[i])
+    print(b)
+
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+even_list(a)
