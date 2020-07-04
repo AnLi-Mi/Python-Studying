87501fc40f89be83fd2cbde4b6ac0f529c85cd5c
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-04 16:02:06 +0200

    added a finction for 'medium password'

--- Project Password Generator.py
+++ Project Password Generator.py
@@ -21,16 +21,53 @@ upper_case = list(s.ascii_uppercase)
 numbers = list(s.digits)
 symbols = list(s.punctuation)
 
-# creating a list of types of characters to choose from
-characters_pull = [lower_case, upper_case, numbers, symbols]
+
 
 import random as r
 
-# function for a strong passowrd
-def pword():
-    
+
+# function for a midium passowrd - 10 charakters, which are mix of upper and lowercase and number
+
+
+def pword_medium():
+    # creating a list of types of characters to choose from
+    characters_pull = [lower_case, upper_case, numbers]
+
     # creating empty list to populat it with chosen elements
     pw=[]
+    # setting a default password lenthg for 10 characters)
+    pw_l=10
+
+    # sequring at least one character of each obligatory type
+    pw.append(r.choice(lower_case))
+    pw.append(r.choice(upper_case))
+    pw.append(r.choice(numbers))
+
+    # randomly selecting types of rest of characters
+    for i in range (4, pw_l+1):
+        # randomly choosing a type of character
+        char=r.choice(characters_pull)
+        # randomly choosing a character from the selected type
+        pw.append(r.choice(char))
+
+    # shuffling all selected elements 
+    r.shuffle(pw)
+
+    # turning a list of elelemts into a string
+    pw="".join(pw)
+    print(f'Twoje hasło to: {pw}')
+
+
+
+# function for a strong passowrd - min 12 characters, which are a mix of:lowercase letters, uppercase letters, numbers, and symbols
+def pword_strong():
+
+    # creating a list of types of characters to choose from
+    characters_pull = [lower_case, upper_case, numbers, symbols]
+
+    # creating empty list to populat it with chosen elements
+    pw=[]
+
     # asking user to choose a length of their password (setting a min lenthg of password 12 characters)
     pw_l=int(input(f"Jaką długość ma mieć hasło (min 12 znaków)?: "))
     # repetedly ask user for a length of the password until it's chosen correctly
@@ -42,18 +79,23 @@ def pword():
     pw.append(r.choice(upper_case))
     pw.append(r.choice(numbers))
     pw.append(r.choice(symbols))
+
     # randomly selecting types of rest of characters
     for i in range (5, pw_l+1):
         # randomly choosing a type of character
         char=r.choice(characters_pull)
-        # randomly choosing a character from the selected typ
+        # randomly choosing a character from the selected type
         pw.append(r.choice(char))
+
     # shuffling all selected elements 
     r.shuffle(pw)
+
     # turning a list of elelemts into a string
     pw="".join(pw)
     print(f'Twoje hasło to: {pw}')
 
-pword()
 
-# jutro: wybrac dlugosc hasla, losownie typow elementow z 4 narzuconymi, users input
+pword_medium()
+
+pword_strong()
+
