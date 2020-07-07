49ea840130c161c3bd4f343de7bc42f10ce93029
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-07 22:42:59 +0200

    describing code

--- Project Requesting Web.py
+++ Project Requesting Web.py
@@ -1,13 +1,26 @@
+
+# Use the BeautifulSoup and requests Python packages to print out
+#a list of all the article titles on the New York Times homepage.
+
+#-------------------SOLUTION----------------
+
 import requests
 from bs4 import BeautifulSoup
 
+#downloading the webpage
 source = requests.get("https://www.nytimes.com/")
 
-source=source.text
-soup = BeautifulSoup(source, 'html.parser')
-#soup=soup.h2
 
+#turning the code into a simple text
+source=source.text
+
+#structuring the text the bs
+soup = BeautifulSoup(source, 'html.parser')
+
+#starting numbering the listed titles
 i=1
+
+#selecting all text that is h2 -> a title of article
 for header in soup.find_all('h2'):
     print(f"Title {i}: '{header.string}'")
     i+=1
