6728e7e841e615a8dcd3bd923f7a4796f8f9eaed
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-27 16:20:41 +0200

    hospital and doctor info extraction based on inserted id

--- Project Hospital DB.py
+++ Project Hospital DB.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+# Question 1: Connect to your database server and print its version
 def connection_start():
     database = mysql.connector.connect(user= 'root', password= '8G13rm3k', host= 'localhost', database='python_db')
     return database
@@ -19,4 +20,39 @@ def version_check():
     connection_stop(database)
 
 
-print(f"I'm using {version_check()} version of MySQL")  
+print(f"Qeestion 1: I'm using {version_check()} version of MySQL")
+
+# Question 2: Fetch Hospital and Doctor Information
+#using hospital Id and doctor Id
+
+def doctor_info(doctor_id):
+    database=connection_start()
+    cursor=database.cursor()
+    doctor_details=("select * from Doctor where Doctor_Id = %s")
+    cursor.execute(doctor_details, (doctor_id,))
+    result = cursor.fetchall()
+   # return print (result)
+    for info in result:
+        print(f'Name: {info[1]}')
+        print(f'Hospital_ID: {info[2]}')
+        print(f'Joined the hospital on: {info[3]}')
+        print(f'Speciality: {info[4]}')
+        print(f'Salary: {info[5]}')
+    connection_stop(database)
+
+def hospital_info(hospital_id):
+    database=connection_start()
+    cursor=database.cursor()
+    doctor_details=("select * from hospital where Hospital_Id = %s")
+    cursor.execute(doctor_details, (hospital_id,))
+    result = cursor.fetchall()
+    for info in result:
+        print(f'Name: {info[1]}')
+        print(f'Number of beds: {info[2]}')
+    connection_stop(database) 
+
+doc=input('Enter in ID number of a doctor: ')
+doctor_info(doc)
+
+hos=input('Enter in ID number of a hospital: ')
+hospital_info(hos)
