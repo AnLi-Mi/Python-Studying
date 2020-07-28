693cc05a792ae59d89c4d59d936e6dbc2222013b
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-28 21:28:31 +0200

    draft of the program

---
+++ Project Counting Days.py
@@ -0,0 +1,16 @@
+# This program generates a date of a day in given number of days.
+# It's usefull for users who need to arrange i.e. doctors check-ups every specific numbers of days.
+
+# Downloading the date module
+
+# Creating a list of a days in a year 
+
+# Asking user to indicate the number of counted days
+
+# Asking user for a starting day
+
+# Extracting the date of a given index
+
+# Extanding the use of the code for upcoming years
+
+# Including leap-years
