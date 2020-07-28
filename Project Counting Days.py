47141d1e389486419bb7e682a66d29f6a1485695
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-28 22:16:59 +0200

    toying with array module

--- Project Counting Days.py
+++ Project Counting Days.py
@@ -3,10 +3,14 @@
 
 # Downloading the date module
 
+import time
+import numpy as np
+
 # Creating a list of a days in a year
 
 jan = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 feb = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+leap_feb = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]
 mar = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 apr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 may = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
@@ -18,10 +22,14 @@ oct = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,2
 nov = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 dec = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 
+months = {1:jan, 2:feb, 3:mar, 4:apr, 5:may, 6:jun, 7:jul, 8:aug, 9:sep, 10:oct, 11:nov, 12:dec}
+
 
 year = jan + feb + mar + apr + may + jun + jul + sep + oct + nov + dec
+year1 = np.array([jan, feb, mar, apr, may, jun, jul, sep, oct, nov, dec])
+leap_year = jan + leap_feb + mar + apr + may + jun + jul + sep + oct + nov + dec
 
-print(year)
+print(year1)
 
 # Asking user to indicate the number of counted days
 
@@ -29,13 +37,16 @@ num = int(input('Enter the number of days you want to count: '))
 
 # Asking user for a starting day
 
-start_date = int(input('Enter the date you want to count from: '))
+start_month = int(input('Enter the month of the day you want to count from: '))
+start_date = int(input('Enter the day you want to count from: '))
+print (year1[start_month, start_date])
 
 # Extracting the day of a given index
 
 day = year [start_date + num-1]
 
-print (day)
+#print (day)
+
 
 # Extracting a month 
 
