1b23b7b609eb740c67274f9c354431cc3417c9e7
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-29 15:11:02 +0200

    default date = today if ValueError

--- Project Counting Days.py
+++ Project Counting Days.py
@@ -10,36 +10,49 @@ from datetime import timedelta
 
 
 # Asking user to indicate the starting date:
-print('Enter the date you want to start counting from')
-start_yr = int(input('year: '))
-start_mo = int(input('month: '))
-start_day = int(input('day: '))
+#print('Enter the date you want to start counting from')
+#start_yr = int(input('year: '))
+#start_mo = int(input('month: '))
+#start_day = int(input('day: '))
 
 
 # Asking user to indicate th number of days they want to calculate:
 
-num = int(input('Enter the number of days you want to count:  '))
+#num = int(input('Enter the number of days you want to count:  '))
 
 
 # converting the input into datetime attributes
+#days = timedelta(days = num)
+
+def create_date(start_yr, start_mo, start_day):
+    
+    try:
+        start_date= date(start_yr, start_mo, start_day)
+        print (start_date)
+        future_date = (start_date + days) 
+        print (f'In {num} days from {start_date} will be {future_date}')
+
+    except ValueError:
+        print ("Incorrect entered date so we will use today's date as a default")
+        start_date=date.today()
+        future_date = (start_date + days) 
+        print (f'In {num} days from {start_date} will be {future_date}')
+  
+
+print('Enter the date you want to start counting from')
+start_yr = int(input('year: '))
+start_mo = int(input('month: '))
+start_day = int(input('day: '))
+num = int(input('Enter the number of days you want to count:  '))
 days = timedelta(days = num)
-
-    # Default today's date
-if start_yr == 0 or start_mo not in (1 ,12) or start_day not in (1,31):
-    start_date=date.today()
-else:
-    #start_yr = int(start_yr)
-    #start_mo = int(start_mo)
-    #start_day = int(start_day)
-    start_date= date(start_yr, start_mo, start_day)
-
 
 
 #generating the future date
 
-future_date = (start_date + days) 
+create_date(start_yr, start_mo, start_day)
 
-print (f'In {num} days from {start_date} will be {future_date}')
+#future_date = (start_date + days) 
+
 
 
 
