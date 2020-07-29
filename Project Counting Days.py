485f5a36a71fb986ceb932c9f14d74c6b863f921
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-29 13:31:05 +0200

    default date =today if input incorect

--- Project Counting Days.py
+++ Project Counting Days.py
@@ -14,16 +14,26 @@ print('Enter the date you want to start counting from')
 start_yr = int(input('year: '))
 start_mo = int(input('month: '))
 start_day = int(input('day: '))
+
+
 # Asking user to indicate th number of days they want to calculate:
 
 num = int(input('Enter the number of days you want to count:  '))
 
 
 # converting the input into datetime attributes
-
-start_date= date(start_yr, start_mo, start_day)
-
 days = timedelta(days = num)
+
+    # Default today's date
+if start_yr == 0 or start_mo not in (1 ,12) or start_day not in (1,31):
+    start_date=date.today()
+else:
+    #start_yr = int(start_yr)
+    #start_mo = int(start_mo)
+    #start_day = int(start_day)
+    start_date= date(start_yr, start_mo, start_day)
+
+
 
 #generating the future date
 
