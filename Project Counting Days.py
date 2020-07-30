f3f16e1c96aebdfd82e7793886b3070eb0295812
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-30 10:52:17 +0200

    simplifying data input - one sting converted to data format using strptime method

--- Project Counting Days.py
+++ Project Counting Days.py
@@ -10,11 +10,11 @@ from datetime import timedelta
 
 # creating a function generating a starting date form given veriables
 #and than adding given number of days
-def create_date(start_yr, start_mo, start_day):
+def create_date(start_date, num):
     
     try:
-        # converting the input into datetime attributes
-        start_date= date(start_yr, start_mo, start_day)
+        #converting the input into timedalta attribute
+        days = timedelta(days = num)
         #generating the future date
         future_date = (start_date + days) 
         print (f'In {num} days from {start_date} will be {future_date}')
@@ -23,40 +23,28 @@ def create_date(start_yr, start_mo, start_day):
         print ("Incorrect entered date so we will use today's date as a default")
         #creating default date if ValueError
         start_date=date.today()
+        #converting the input into timedalta attribute
+        days = timedelta(days = num)
         #generating the future date
         future_date = (start_date + days) 
         print (f'In {num} days from {start_date} will be {future_date}')
   
 # asking user to indicate th number of days they want to calculate:
 
-print('Enter the date you want to start counting from')
+date_str = input('Enter the date you want to start counting from (dd/mm/yyy): ' )
 
-# get year:
-try:
-    start_yr = int(input('year: '))
-except ValueError:
-    start_yr = int(input('Error! Please enter a correct value. Year: '))
-     
-# get month:
-try:
-    start_mo = int(input('month: '))
-except ValueError:
-    start_mo = int(input('Error! Please enter a correct value. Month: '))
+#convering a given string into date format
+from datetime import datetime
+start_date = datetime.strptime(date_str, "%d/%m/%Y")
 
-# get day:
-try:
-    start_day = int(input('day: '))
-except ValueError:
-    start_day = int(input('Error! Please enter a correct value. Day: '))
+
 
 # asking user to indicate th number of days they want to calculate:
 num = int(input('Enter the number of days you want to count:  '))
 
-#converting the input into timedalta attribute
-days = timedelta(days = num)
 
 #calling the function with given virables
-create_date(start_yr, start_mo, start_day)
+create_date(start_date, num)
 
  
 
