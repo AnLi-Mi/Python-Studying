bbedaf6313a50168195c7b37d2d31fb340ee94df
AnLi-Mi <a.lipiarz@gmail.com>
2020-07-31 15:17:37 +0200

    testing inserting values into pg db via python

--- demo_pg_test.py
+++ demo_pg_test.py
@@ -3,14 +3,20 @@ mypgdb=psycopg2.connect(user = "postgres", password = "8G13rm3k", host = "localh
 
 p_cursor = mypgdb.cursor()
 
-#connection test
+    #connection test
 
 con_test = mypgdb.get_dsn_parameters()
 print(con_test)
 
-#craeting a new table - testing changing/updating commands
+    #craeting a new table - testing changing/updating commands
 
-new_table = ("CREATE TABLE Students (Student_ID int, Name varchar(50), Surname varchar(50), Faculty varchar(50));")
+#new_table = ("CREATE TABLE Students (Student_ID int, Name varchar(50), Surname varchar(50), Faculty varchar(50));")
 
-p_cursor.execute(new_table)
+#p_cursor.execute(new_table)
+#mypgdb.commit()
+
+    #populating 'customer' table with new records
+
+add_customers = ("INSERT INTO customers (customer_id, customer_name, email) VALUES (6, 'Matt Smith', 'matt@smith.com');")
+p_cursor.execute(add_customers)
 mypgdb.commit()
