60cfe8cb67c12908e47adc399cbc8943bf8acac7
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-03 15:25:23 +0200

    correcting typos and minor syntax mistaces

--- anna_module.py
+++ anna_module.py
@@ -6,13 +6,13 @@ def greeting(name):
 def correct_input_request(guess,pull):
         
         # ponownie zapytanie az uzytkownik poprawnie wpisze swoj wybor
-        while guess NOT IN pull:
-            guess = input(f'Wrong input, please choose again from {pull}: '))
+        while guess not in pull:
+            guess = input(f'Wrong input, please choose again from {pull}: ')
 
 
 
 numbers_pull = range(1,11)
 
-animal_pull = [cat, dog, lion, parrot, snake, rabbit, graffe, mouse, pig, donkey]
+animal_pull = ["cat", "dog", "lion", "parrot", "snake", "rabbit", "graffe", "mouse", "pig", "donkey"]
 
-srp_pull = [scisors, rock, paper]
+srp_pull = ["scisors", "rock", "paper"]
