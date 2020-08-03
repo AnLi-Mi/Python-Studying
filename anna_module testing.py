2f0f6fd5e60d071a09fba5b527a5bae6577b2a9d
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-03 16:17:30 +0200

    fixing problem with diferent type of pull elements and type of input)

--- anna_module testing.py
+++ anna_module testing.py
@@ -1,20 +1,23 @@
 import anna_module
 
+print(dir(anna_module))
+
 name = input(f"What's your name? ")
 
 anna_module.greeting(name)
 
-#importing pull for the rock-paper-scisors game
-pull = anna_module.srp_pull
+#importing specific pull for the  game
+pull = anna_module.numbers_pull
 
 #randomly selecting one item from the imported list 
 guess = anna_module.random_selection(pull)
-print(guess)
 
-choice = input(f'Choose one item from: {pull}')
-anna_module.correct_input_request(choice,pull)
+#requesting input from the user
+choice = int(input(f'Choose one item from: {pull}'))
+#impementing imported function to request typing correct value
+anna_module.correct_int_input_request(choice,pull)
 
-
-anna_module.comparing(guess, choice)
+#showing the result
+anna_module.guess_comparing(guess, choice)
 
 
--- anna_module.py
+++ anna_module.py
@@ -2,11 +2,15 @@ def greeting(name):
     print (f'Hello {name.capitalize()}, welcome in the game!')
 
 
-def correct_input_request(guess,pull):
+def correct_int_input_request(guess,pull):
         # ponownie zapytanie az uzytkownik poprawnie wpisze swoj wybor
         while guess not in pull:
-            guess = input(f'Wrong input, please choose again from {pull}: ')
+            guess = int(input(f'Wrong input, please choose again from {pull}: '))
 
+def correct_str_input_request(guess,pull):
+        # ponownie zapytanie az uzytkownik poprawnie wpisze swoj wybor
+        while guess not in pull:
+            guess = input(f'Wrong input, please choose again from {pull}: ')            
 
 def random_selection(pull):
     #losowanie pojedynczego elementu z podanej listy
@@ -15,13 +19,18 @@ def random_selection(pull):
     return selection
 
 
-def comparing(comp, user):
+def guess_comparing(comp, user):
     if user == comp:
         print ('Congrats! You guessed!')
     else:
         print (f'You lost, the selected item was {comp}.') 
 
-
+def guess_comparing(comp, user):
+    if user == comp:
+        print ('Congrats! You guessed!')
+    else:
+        print (f'You lost, the selected item was {comp}.')
+        
 #przykłady list
 numbers_pull = range(1,11)
 
