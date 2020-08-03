61572a55ee3a1631dfdc51d015d3060696cc21a7
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-03 15:47:20 +0200

    creating a guessing game made out of elements of my module

--- anna_module testing.py
+++ anna_module testing.py
@@ -1,12 +1,20 @@
 import anna_module
 
-anna_module.greeting("Anna")
+name = input(f"What's your name? ")
 
+anna_module.greeting(name)
 
+#importing pull for the rock-paper-scisors game
 pull = anna_module.srp_pull
-choice = input(f'Choose one item from: {pull}')
 
+#randomly selecting one item from the imported list 
+guess = anna_module.random_selection(pull)
+print(guess)
+
+choice = input(f'Choose one item from: {pull}')
 anna_module.correct_input_request(choice,pull)
 
+
+anna_module.comparing(guess, choice)
 
 
