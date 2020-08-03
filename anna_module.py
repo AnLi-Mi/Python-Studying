1997448db382db004e88ec54b65286c5b92deec2
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-03 15:36:23 +0200

    adding a random selection function so the consists necesaery elements for the guessing games

--- anna_module.py
+++ anna_module.py
@@ -1,16 +1,21 @@
 def greeting(name):
-
     print (f'Hello {name.capitalize()}!')
 
 
 def correct_input_request(guess,pull):
-        
         # ponownie zapytanie az uzytkownik poprawnie wpisze swoj wybor
         while guess not in pull:
             guess = input(f'Wrong input, please choose again from {pull}: ')
 
 
+def random_selection(pull):
+    #losowanie pojedynczego elementu z podanej listy
+    import random
+    selection = random.choice(pull)
+    return selection
 
+
+#przykłady list
 numbers_pull = range(1,11)
 
 animal_pull = ["cat", "dog", "lion", "parrot", "snake", "rabbit", "graffe", "mouse", "pig", "donkey"]
