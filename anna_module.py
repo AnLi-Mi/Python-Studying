2bb8afa1039f815b7b981ef742d1dfb4b312aff6
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-03 15:20:26 +0200

    testing creation of own module: mix of functions and variables

---
+++ anna_module.py
@@ -0,0 +1,18 @@
+def greeting(name):
+
+    print (f'Hello {name.capitalize()}!')
+
+
+def correct_input_request(guess,pull):
+        
+        # ponownie zapytanie az uzytkownik poprawnie wpisze swoj wybor
+        while guess NOT IN pull:
+            guess = input(f'Wrong input, please choose again from {pull}: '))
+
+
+
+numbers_pull = range(1,11)
+
+animal_pull = [cat, dog, lion, parrot, snake, rabbit, graffe, mouse, pig, donkey]
+
+srp_pull = [scisors, rock, paper]
