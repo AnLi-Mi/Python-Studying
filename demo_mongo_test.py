dd321fdb9ed62f10a758d41001818f32ac357b46
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-05 11:34:33 +0200

    sucessfully removing all duplicted docs

--- demo_mongo_test.py
+++ demo_mongo_test.py
@@ -1,11 +1,11 @@
-#downloading necessary module, connecting to server and checking connection
+#---downloading necessary module, connecting to server and checking connection---
 
 import pymongo
 connect = pymongo.MongoClient("mongodb://localhost:27017/")
 print(pymongo.server_description)
 
 
-#checking existing databases and creating a new one
+#---checking existing databases and creating a new one--------
 
 db_list =connect.list_database_names()
 print(db_list)
@@ -15,7 +15,7 @@ if "python_db" in db_list:
 
 first_db =connect["python_db"]
 
-#creating a collection in a new db
+#---creating a collection in a new db-------------
 
 db_col =first_db.list_collection_names()
 print(db_col)
@@ -26,7 +26,7 @@ if "clients" in db_col:
 first_col =first_db["clients"]
 
 
-#creating a dictionary with information/documents' content I want to include in the collection
+#---creating a dictionary with information/documents' content I want to include in the collection
 
 mydocs= [{ "name": "Amy", "address": "Apple st 652"},
   { "name": "Hannah", "address": "Mountain 21"},
@@ -41,28 +41,28 @@ mydocs= [{ "name": "Amy", "address": "Apple st 652"},
   { "name": "Chuck", "address": "Main Road 989"},
   { "name": "Viola", "address": "Sideway 1633"}]
 
-#inserting the list of dictionaries as documents to my collection
+#----inserting the list of dictionaries as documents to my collection-----
 
 # # insert = first_col.insert_many(mydocs)
 # #print(insert)
 
-# extracting all data from database
+# ---extracting all data from database-------------------
 
 for doc in first_col.find():
     print(doc)
 
-# excluting 'address' from extraction
+# ---excluting 'address' from extraction------------------------
 
 for doc in first_col.find({},{"address":0}):
     print(doc)
 
-# extracting one/fisrt document from database
+# ---extracting one/fisrt document from database-----------------------
 
 ext_one = first_col.find_one()
 print(ext_one)
 
 
-# extracting filtered documents form the colelction (using a query)
+# ---extracting filtered documents form the colelction (using a query)-----
 
 query1 = {"name":"Betty"}
 query2 = {"address": "/Valley/" }
@@ -81,28 +81,30 @@ for doc in first_col.find(query3):
     print(doc)
 
 
-# sorting resulte dsc
+# -----------sorting results dsc--------------------
 
-print ('----------SORTING Q3----------\n')
 for doc in first_col.find(query3).sort("name",-1):
     print(doc)
 
-# deleting duplicated documents
 
-count_query={"name":"Sandy"}
-del_query={"name":"Sandy"}
+# ----------deleting duplicated documents-------------
+# choosing a doc
 
-viola_count = first_col.find(count_query).count()
-print(viola_count)
+query={"first_name":"Antol"}
 
+# counting how many times the doc is repeated
+doc_count = first_col.find(query).count()
+print(doc_count)
+
+#looping a deleting function until only one doc is left
 i=1
-y=int(viola_count)
+y=int(doc_count)
 
-while 1<(y-2):
-    first_col.delete_one(del_query)
+while i<y:
+    first_col.delete_one(query)
     i+=1
 
-for doc in first_col.find(query3).sort("name",-1):
+for doc in first_col.find().sort("name",-1):
     print(doc)
     
 
