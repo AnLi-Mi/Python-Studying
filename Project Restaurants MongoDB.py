938031c2546cd5bf62b6c35fef7b084ab4e592d4
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-05 15:44:44 +0200

    reslolivng few tasks involving extracting specyfic fieldsfrom filtered, skipped and limitted documents

--- Project Restaurants MongoDB.py
+++ Project Restaurants MongoDB.py
@@ -12,16 +12,48 @@ print(col_list)
 
 col=db["restaurants"]
 
-# Question 1: Write a MongoDB query
-# to display all the documents in the collection restaurants.
-
 print('----------------------------Q1-----------------------------------')
 
-all_doc=col.find()
+# Question 1: Write a MongoDB query
+# to display all the documents in the collection restaurants. limit to 10 first results
+
+all_doc=col.find().limit(10)
 
 for res in all_doc:
-   print(res)
+    print(res)
+
+print('----------------------------Q2-----------------------------------')
+
+# Question 2: Write a MongoDB query to display the fields
+#restaurant_id, name, borough and cuisine for all the documents. limit to 10 first results
+
+all_doc=col.find().limit(10)
+
+for res in all_doc:
+   print(f'{res["restaurant_id"]}, {res[ "name"]}, {res[ "borough"]}, {res[ "cuisine"]}')
 
 
-  
+print('----------------------------Q3-----------------------------------')
+
+#  Write a MongoDB query to display names of all the restaurant
+# which is in the borough Bronx. limit to 10 first results
+
+query={"borough":"Bronx"}
+
+bronx_res = col.find(query).limit(10)
+
+for res in bronx_res:
+    print(res["name"])
+
+print('----------------------------Q4-----------------------------------')
+
+#Write a MongoDB query to display the next 5 restaurants
+#after skipping first 5 which are in the borough Bronx
+
+query={"borough":"Bronx"}
+
+bronx_res = col.find(query).skip(5).limit(5)
+
+for res in bronx_res:
+    print(res["name"])
 
