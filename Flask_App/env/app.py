b43cbf66ee74199e5fe39d35bb4df66642ab01fb
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-10 22:08:54 +0200

    connecting python with a simple html code@

--- Flask_App/env/app.py
+++ Flask_App/env/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/<a>')
-def index(a):
-    return"<h1> I'm Anna {}!</h1>".format(a)
+@app.route('/<name>')
+def index(name):
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
