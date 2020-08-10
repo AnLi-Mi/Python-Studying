d0fd7a662d868845f8db5fbe9e14a5067e5b3e62
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-10 22:22:09 +0200

    creating my first template

--- Flask_App/env/app.py
+++ Flask_App/env/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/<name>')
-def index(name):
+@app.route('/')
+def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
