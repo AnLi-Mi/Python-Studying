521b99904f3b7876a81ecec5de20f4ab0e4378ce
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-10 20:01:26 +0200

    initial editing

--- Flask_App/env/app.py
+++ Flask_App/env/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return"<h1> I'm Anna </h1>"
+@app.route('/<a>')
+def index(a):
+    return"<h1> I'm Anna {}!</h1>".format(a)
