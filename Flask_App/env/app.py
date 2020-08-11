4257280f064724147e1b335c1ec0213bd3f84ce0
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-11 19:42:54 +0200

    adding fnacy backet_list page with given html and linked css

--- Flask_App/env/app.py
+++ Flask_App/env/app.py
@@ -2,17 +2,23 @@ from flask import Flask, render_template,url_for
 
 app=Flask(__name__)
 
+@app.route('/bucket_list')
+def main():
+    return render_template('bucket_list.html')
+
 @app.route('/home')
 def hello():
     return "<h1> Welcome on my page!<h1>"
 
 @app.route('/about')
-def main():
-    return render_template('index.html')
+def about():
+    return render_template('about.html')
 
 @app.route('/login')
-def index():
+def login():
     return render_template('login.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
