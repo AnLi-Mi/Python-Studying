3243a6ec4ac785a00e70e2bd1bc2d3459f7bd797
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-11 19:17:37 +0200

    adding multiple pages to my webapp

--- Flask_App/env/app.py
+++ Flask_App/env/app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template,url_for
 
 app=Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/home')
+def hello():
+    return "<h1> Welcome on my page!<h1>"
+
+@app.route('/about')
+def main():
     return render_template('index.html')
+
+@app.route('/login')
+def index():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
