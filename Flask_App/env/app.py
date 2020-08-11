62aa5591d5e6039e79ecfc8f8888a1b8419217ad
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-11 20:56:47 +0200

    i have n o idea what i've done, i will get back to it tomorrow

--- Flask_App/env/app.py
+++ Flask_App/env/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for, request, json
 import mysql.connector
 from mysql.connector import Error
 
@@ -25,6 +25,20 @@ def showSignUp():
 if __name__ == "__main__":
     app.run(debug=True)
 
+@app.route('/signUp', methods=['POST'])
+def signUp():
+     # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    
 
 
 
