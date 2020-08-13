d6fc07bb00b7a567e0daa83cad243164241c8ac0
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-13 19:13:29 +0200

    fixing a bug (float input) using a function

--- BMI Flask_app/app.py
+++ BMI Flask_app/app.py
@@ -5,16 +5,22 @@ app=Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def bmi():
-    
     name = ''
-    weight =0
-    hight =0
+    bmi= ''
     if request.method=='POST' and 'username' in request.form:
         name = request.form.get('username')
         w = float(request.form.get('userw'))
         h = float(request.form.get('userh'))
-        bmi = round(w/(h**2),2)
-    return render_template('bmi.html',  name=name, w=w, h=h, bmi=bmi)
+        bmi = calc_bmi(w,h)
+    return render_template('bmi.html',  name=name, bmi=bmi)
+
+def calc_bmi(w,h):
+    return round(w/(h**2),2)
+
+
+@app.route('/test')
+def test():
+    return "testuje!"
 
 if __name__ == "__main__":
     app.run(debug=True)
