60ff55b7756b3c718514578bb7fde5fb6e390509
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-13 20:03:45 +0200

    added another function with recomentations, depanding on the bmi score

--- BMI Flask_app/app.py
+++ BMI Flask_app/app.py
@@ -7,15 +7,25 @@ app=Flask(__name__)
 def bmi():
     name = ''
     bmi= ''
+    re=''
     if request.method=='POST' and 'username' in request.form:
         name = request.form.get('username')
         w = float(request.form.get('userw'))
         h = float(request.form.get('userh'))
         bmi = calc_bmi(w,h)
-    return render_template('bmi.html',  name=name, bmi=bmi)
+        re = rec(bmi)
+    return render_template('bmi.html',  name=name, bmi=bmi, re=re)
 
 def calc_bmi(w,h):
     return round(w/(h**2),2)
+
+def rec(bmi):
+    if bmi<18.5:
+        return "You're too skinny"
+    elif bmi>25:
+        return "You need to loose some weight"
+    else:
+        return "You have correct weight"
 
 
 @app.route('/test')
