01bac3041fe6db2cce64bb22c0bc8192583ed0d1
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-13 16:20:41 +0200

    fixing a bug in bmi calculator

--- BMI Flask_app/app.py
+++ BMI Flask_app/app.py
@@ -8,13 +8,13 @@ def bmi():
     
     name = ''
     weight =0
-    #hight =0
+    hight =0
     if request.method=='POST' and 'username' in request.form:
         name = request.form.get('username')
-        weight = int(request.form.get('userw'))
-      #  hight= int(request.form.get('userh'))
-       # BMI = weight/(hight**2)
-    return render_template('bmi.html',  name=name, weight=weight)
+        w = int(request.form.get('userw'))
+        h = int(request.form.get('userh'))
+        bmi = w/(h**2)
+    return render_template('bmi.html',  name=name, w=w, h=h, bmi=bmi)
 
 if __name__ == "__main__":
     app.run(debug=True)
