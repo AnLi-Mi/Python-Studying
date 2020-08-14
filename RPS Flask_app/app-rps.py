dfcacfdeb3825441ad330753219f949dc6e84f5a
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-14 14:44:20 +0200

    py part of the code

--- RPS Flask_app/app-rps.py
+++ RPS Flask_app/app-rps.py
@@ -2,16 +2,38 @@
 
 from flask import Flask, request, render_template
 import random
+from anna_module import greeting, srp_pull, rps_comparing, random_selection
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def game():
-    return "It will be a rock-paper-scisores game"
+    name=''
+    choice=''
+    comp=''
+    result=''
+    if request.method=='POST' and 'userchoice' in request.form:
+        name=request.form.get('username')
+        choice=request.form.get('userchoice')
+        comp=random_selection(srp_pull)
+        result=rps_comparing(comp, choice)
+    return render_template('index.html', name=name, result=result, comp=comp)
     
-
-
 app.run()
+
+
+#name=input('Enter your name')
+
+#greeting(name)
+
+#choice=input('What are you choosing - rock, paper or scisors?: ')
+
+#comp = random_selection(srp_pull)
+
+#rps_comparing(comp, choice)
+
+
+
 
 
 
