67945bce2a89b95582cc4188302334ee4ea3c6ae
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-14 14:56:13 +0200

    html part of code

---
+++ RPS Flask_app/anna_module.py
@@ -0,0 +1,47 @@
+def greeting(name):
+    print (f'Hello {name.capitalize()}, welcome in the game!')
+
+
+def correct_int_input_request(guess,pull):
+        # ponownie zapytanie az uzytkownik poprawnie wpisze swoj wybor
+        while guess not in pull:
+            guess = int(input(f'Wrong input, please choose again from {pull}: '))
+
+def correct_str_input_request(guess,pull):
+        # ponownie zapytanie az uzytkownik poprawnie wpisze swoj wybor
+        while guess not in pull:
+            guess = input(f'Wrong input, please choose again from {pull}: ')            
+
+def random_selection(pull):
+    #losowanie pojedynczego elementu z podanej listy
+    import random
+    selection = random.choice(pull)
+    return selection
+
+
+def guess_comparing(comp, user):
+    if user == comp:
+        print ('Congrats! You guessed!')
+    else:
+        print (f'You lost, the selected item was {comp}.') 
+
+def guess_comparing(comp, user):
+    if user == comp:
+        print ('Congrats! You guessed!')
+    else:
+        print (f'You lost, the selected item was {comp}.')
+
+def rps_comparing(comp, user):
+    if user.lower() == comp:
+        print(f'Remis! ')
+    elif (user.lower() == "paper" and comp== "rock") or (user.lower() == "rock" and comp== "scisors") or (user.lower() == "scisors" and comp== "paper")  :
+        print(f'You won! ')
+    else:
+        print(f'Coputer selected {comp} - you lost!')
+        
+#przykłady list
+numbers_pull = range(1,11)
+
+animal_pull = ["cat", "dog", "lion", "parrot", "snake", "rabbit", "graffe", "mouse", "pig", "donkey"]
+
+srp_pull = ["scisors", "rock", "paper"]
