d8622cc9772cc2d0f54fe23d3d17ec492504a82b
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-15 16:41:29 +0200

    testing flaskext.mysql and connecting flask with python_db database

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,47 +1,76 @@
 from flask import Flask, render_template,url_for, request, json
-import mysql.connector
-from mysql.connector import Error
+from flaskext.mysql import MySQL
 
 app=Flask(__name__)
+mysql= MySQL()
 
-@app.route('/', methods=['GET', 'POST'])
-def start():
-    name = ''
-    age =0
-    fut =0
-    if request.method=='POST' and 'username' in request.form:
-        name = request.form.get('username')
-        age = int(request.form.get('userage'))
-        fut= (age+5)   
-    return render_template('index.html', name=name, age=age, fut=fut)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '8G13rm3k'
+app.config['MYSQL_DATABASE_DB'] = 'python_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
-@app.route('/bucket_list')
-def main():
-    return render_template('bucket_list.html')
+#@app.route('/', methods=['GET', 'POST'])
+#def start():
+  #  name = ''
+   # age =0
+    #fut =0
+   # if request.method=='POST' and 'username' in request.form:
+    #    name = request.form.get('username')
+     #   age = int(request.form.get('userage'))
+      #  fut= (age+5)   
+   # return render_template('index.html', name=name, age=age, fut=fut)
 
-@app.route('/home')
-def hello():
-    return "<h1> Welcome on my page!<h1>"
+#@app.route('/bucket_list')
+#def main():
+ #   return render_template('bucket_list.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+#@app.route('/home')
+#def hello():
+ #   return "<h1> Welcome on my page!<h1>"
 
-@app.route('/showSignUp')
-def showSignUp():
-    return render_template('signup.html')
+#@app.route('/about')
+#def about():
+ #   return render_template('about.html')
+
+#@app.route('/showSignUp')
+#def showSignUp():
+ #   return render_template('signup.html')
+
+#@app.route('/method', methods= ['GET','POST'])
+#def met():
+#    if request.method == 'POST':
+#        return "I'm using POST method"
+#    else:
+#        return "I'm using GET method"
 
 
-@app.route('/method', methods= ['GET','POST'])
-def met():
-    if request.method == 'POST':
-        return "I'm using POST method"
-    else:
-        return "I'm using GET method"
+
+@app.route('/data')
+def database():
+    results=''
+    query = "SELECT * FROM hospital"
+    results = executing_query(query)
+    return render_template('data.html', results=results)
+
+
+def executing_query(query):
+    conn = mysql.connect()
+    cursor =conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return results
+
+
+
 
     
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+   app.run(debug=True)
+
+    
+
 
 
 
