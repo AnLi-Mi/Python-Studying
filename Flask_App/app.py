053fdf05cc143e4e16ac0a8ad6ce192f4db4ea5d
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-15 16:57:05 +0200

    toying with html desplay of the db results

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -10,39 +10,39 @@ app.config['MYSQL_DATABASE_DB'] = 'python_db'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-#@app.route('/', methods=['GET', 'POST'])
-#def start():
-  #  name = ''
-   # age =0
-    #fut =0
-   # if request.method=='POST' and 'username' in request.form:
-    #    name = request.form.get('username')
-     #   age = int(request.form.get('userage'))
-      #  fut= (age+5)   
-   # return render_template('index.html', name=name, age=age, fut=fut)
+@app.route('/', methods=['GET', 'POST'])
+def start():
+    name = ''
+    age =0
+    fut =0
+    if request.method=='POST' and 'username' in request.form:
+        name = request.form.get('username')
+        age = int(request.form.get('userage'))
+        fut= (age+5)   
+    return render_template('index.html', name=name, age=age, fut=fut)
 
-#@app.route('/bucket_list')
-#def main():
- #   return render_template('bucket_list.html')
+@app.route('/bucket_list')
+def main():
+    return render_template('bucket_list.html')
 
-#@app.route('/home')
-#def hello():
- #   return "<h1> Welcome on my page!<h1>"
+@app.route('/home')
+def hello():
+    return "<h1> Welcome on my page!<h1>"
 
-#@app.route('/about')
-#def about():
- #   return render_template('about.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-#@app.route('/showSignUp')
-#def showSignUp():
- #   return render_template('signup.html')
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
 
-#@app.route('/method', methods= ['GET','POST'])
-#def met():
-#    if request.method == 'POST':
-#        return "I'm using POST method"
-#    else:
-#        return "I'm using GET method"
+@app.route('/method', methods= ['GET','POST'])
+def met():
+    if request.method == 'POST':
+        return "I'm using POST method"
+    else:
+        return "I'm using GET method"
 
 
 
