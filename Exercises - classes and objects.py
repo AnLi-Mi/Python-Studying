a981436cadb397906375a2ffbddabb3a0a2e1d22
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-19 19:25:34 +0200

    exercisexercising classes and objects - exercise 2 displaying single lines of a song

--- Exercises - classes and objects.py
+++ Exercises - classes and objects.py
@@ -1,13 +1,12 @@
 # Exercise 1 - Check if it's triangle:
 
-class Triangle:
+class Triangle():
     def __init__ (self, angle1, angle2, angle3):
         self.angle1=angle1
         self.angle2=angle2
         self.angle3=angle3
 
     number_of_sides = 3
-
 
     def check_angles(self):
         if (self.angle1 + self.angle2 + self.angle3) == 180:
@@ -17,7 +16,29 @@ class Triangle:
 
 my_triangle=Triangle(30,60,90)
 
-my_triangle.check_angles()         
+my_triangle.check_angles()
+
+print (my_triangle.number_of_sides)
+
+print ('--------------------------------------------------------------')
+
+# Exercise 2 Printing lines of song - like karaooke
+
+
+class Songs():
+    def __init__(self, lyrics):
+        self.lyrics= lyrics
+
+
+    def sing_me_a_song(self):
+        for l in self.lyrics:
+            print (f'{l} ! ') 
+
+
+happy_birthday=Songs(["May god bless you, ","Have a sunshine on you,","Happy Birthday to you !"])
+
+happy_birthday.sing_me_a_song()
+            
 
 
 
