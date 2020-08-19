8c5f8df43de234864d3d1672ef6c7e63dc389f24
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-19 20:08:49 +0200

    exercising classes and objects - exercise 5 preparing a function turning intiger to roman number"

--- Exercises - classes and objects.py
+++ Exercises - classes and objects.py
@@ -80,6 +80,41 @@ my_point=Point3D(1,2,3)
 
 print(my_point)
 
+print ('--------------------------------------------------------------')
+
+# Exercise 5 Turning intiger in to roman number
+
+
+
+def int_rom_conv(number):
+
+    rom=[]
+    int_rom = {1:'I',
+               4:'IV',
+               5:'V',
+               9: 'IX',
+               10:'X',
+                40:'XL',
+                50:'L',
+                90:'XC',
+                100:'C',
+                400:'CD',
+                500:'D',
+                900:'CM',
+                1000:'M'}
+
+    for n in number:
+        n=int(n)
+        rom.append(int_rom[n])
+
+    print (rom)
+    
+             
+int_rom_conv("1555")
+
+
+            
+
 
 
 
