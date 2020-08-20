e9caec0ba7ad26cf3ab4e581da0e77a1859310d6
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-20 20:19:35 +0200

    adding other functions to test

--- test_exercise.py
+++ test_exercise.py
@@ -5,4 +5,16 @@ class TestBase(unittest.TestCase):
 
     def test_area(self):
         result = unittest_exer_base.area(5)
-        self.assertEqual(result, 78.54 ) 
+        self.assertEqual(result, 78.54 )
+
+    def test_perimeter(self):
+        result = unittest_exer_base.perimeter(5)
+        self.assertEqual(result, 31.42 )
+        
+    def test_area_ins_reg_hex(self):
+        result = unittest_exer_base.area_ins_reg_hex(5)
+        self.assertEqual(result, 64.95)
+
+    def test_area_des_reg_hex(self):
+        result = unittest_exer_base.area_des_reg_hex(5)
+        self.assertEqual(result, 86.6 )
