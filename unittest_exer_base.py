bc1c8bc7f1da05bf35ed656cbb6549fb902edff1
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-20 20:06:29 +0200

    fixing the base code for testing

--- unittest_exer_base.py
+++ unittest_exer_base.py
@@ -1,19 +1,28 @@
 def area(r):
     import math
     area_c= (math.pi)*(r**2)
-    return "The area of this circele is {} square units".format(round(area,2))
+    x = round(area_c,2)
+    return   print(x)
 
 def perimeter(r):
     import math
     perimeter_c= 2*(math.pi)*r
-    return "The perimeter of this circele is {} units".format(round(perimeter,2))
+    x = round(perimeter_c,2)
+    return print(x)
 
 def area_ins_reg_hex(r):
     import math
     area_ins_hex =(3*(r**2)*(math.sqrt(3)))/2
-    return "The area of a regular hexagon inscribed in this circele is {} square units".format(round(area_ins_hex,2))
+    x = round(area_ins_hex,2)
+    return print(x) 
 
 def area_des_reg_hex(r):
     import math
     area_des_hex =2*(r**2)*(math.sqrt(3))
-    return "The area of a regular hexagon desscribed on this circele is {} square units".format(round(area_des_hex,2))
+    x = round(area_des_hex,2)
+    return print(x)
+
+area(5)
+perimeter(5)
+area_ins_reg_hex(5)
+area_des_reg_hex(5)
