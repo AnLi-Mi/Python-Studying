b4a4712c26a2d54db2dc190ffbc2d5fd16d9c2a8
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-20 20:26:59 +0200

    toying with usnittest with methods...

--- Exercises_classes_and_objects.py
+++ Exercises_classes_and_objects.py
@@ -186,22 +186,27 @@ class Circle:
         import math
         area = (math.pi)*(self.radius**2)
         print ("The area of this circele is {} square units".format(round(area,2)))
-
+        return area
+    
     def perimeter(self):
         import math
         perimeter = 2*(math.pi)*self.radius
         print ("The perimeter of this circele is {} units".format(round(perimeter,2)))
-
+        return perimeter
+    
     def area_ins_reg_hex(self):
         import math
         area_ins_hex =(3*(self.radius**2)*(math.sqrt(3)))/2
         print ("The area of a regular hexagon inscribed in this circele is {} square units".format(round(area_ins_hex,2)))
-
+        return area_ins_hex
+    
     def area_des_reg_hex(self):
         import math
         area_des_hex =2*(self.radius**2)*(math.sqrt(3))
         print ("The area of a regular hexagon desscribed on this circele is {} square units".format(round(area_des_hex,2)))    
+        return area_des_hex
 
+    
 circ1 = Circle(2)
 circ1.area()
 circ1.perimeter()
--- test_exercise.py
+++ test_exercise.py
@@ -1,20 +1,20 @@
 import unittest
-import unittest_exer_base
+import Exercises_classes_and_objects.py as e
 
 class TestBase(unittest.TestCase):
 
     def test_area(self):
-        result = unittest_exer_base.area(5)
+        result = e.area(5)
         self.assertEqual(result, 78.54 )
 
     def test_perimeter(self):
-        result = unittest_exer_base.perimeter(5)
+        result = e.perimeter(5)
         self.assertEqual(result, 31.42 )
         
     def test_area_ins_reg_hex(self):
-        result = unittest_exer_base.area_ins_reg_hex(5)
+        result = e.area_ins_reg_hex(5)
         self.assertEqual(result, 64.95)
 
     def test_area_des_reg_hex(self):
-        result = unittest_exer_base.area_des_reg_hex(5)
+        result = e.area_des_reg_hex(5)
         self.assertEqual(result, 86.6 )
