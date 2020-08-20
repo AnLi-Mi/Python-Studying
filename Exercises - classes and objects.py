dafd85a1dadc9a8268e93461e5c642455df209af
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-20 16:57:59 +0200

    finishing up the dictioanry so that it caters any entered year

--- Exercises - classes and objects.py
+++ Exercises - classes and objects.py
@@ -87,16 +87,31 @@ print ('--------------------------------------------------------------')
 class Years:
 
     int_to_rom_dic = {1:'I',
+                      2: 'II',
+                      3: 'III',
                        4:'IV',
                       5: 'V',
+                      6: 'VI',
+                      7: 'VII',
+                      8: 'VIII',
                       9:'IX',
                        10:'X',
+                      20:'XX',
+                      30:'XXX',
                     40:'XL',
                     50:'L',
+                      60:'LX',
+                      70:'LXX',
+                      80:'LXXX',
                     90:'XC',
                     100:'C',
-                    400:'CD',
+                      200:'CC',
+                      300:'CCC',
+                      400:'CD',
                     500:'D',
+                      600:'DC',
+                      700:'DCC',
+                      800:'DCCC',
                     900:'CM',
                    1000:'M'}
 
@@ -105,7 +120,7 @@ class Years:
     
 
     def int_rom_conv(self):
-        
+              
         rom=[] # empty list to populate it with with roman numbers
         elements=[] # empty list to populate with the elements of the sting to turn them to roman numbers
         a=1000 # used to indicate decimal level of a given element 
@@ -129,6 +144,10 @@ class Years:
 
 year1=Years('1555')
 year1.int_rom_conv()
+
+year2=Years('1919')
+Years.int_rom_conv(year2)
+
     
              
 
