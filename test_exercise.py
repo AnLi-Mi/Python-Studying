47c2cf45d38244ec292b0aa062627109783deafd
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-21 16:41:40 +0200

    trying an  option with creating an object within the testing function

--- test_exercise.py
+++ test_exercise.py
@@ -3,18 +3,22 @@ import unittest_exer_base as u
 
 class TestBase(unittest.TestCase):
 
-    def test_area(self, Circle.radius):
-        result = u.Circle.area(5)
+    def test_area(self):
+        obj1 = u.Circle(5)
+        result = u.Circle.area(obj1)
         self.assertEqual(result, 78.54 )
 
     def test_perimeter(self):
-        result = u.Circle.perimeter(5)
+        obj1 = u.Circle(5)
+        result = u.Circle.perimeter(obj1)
         self.assertEqual(result, 31.42 )
         
     def test_area_ins_reg_hex(self):
-        result = u.Circle.area_ins_reg_hex(5)
+        obj1 = u.Circle(5)
+        result = u.Circle.area_ins_reg_hex(obj1)
         self.assertEqual(result, 64.95)
 
     def test_area_des_reg_hex(self):
-        result = u.Circle.area_des_reg_hex(5)
+        obj1 = u.Circle(5)
+        result = u.Circle.area_des_reg_hex(obj1)
         self.assertEqual(result, 86.6 )
--- unittest_exer_base.py
+++ unittest_exer_base.py
@@ -8,6 +8,7 @@ class Circle:
     def area(self):
         import math
         area = (math.pi)*(self.radius**2)
+        area = round(area,2)
         print ("The area of this circele is {} square units".format(round(area,2)))
         return area
     
@@ -15,16 +16,28 @@ class Circle:
         import math
         perimeter = 2*(math.pi)*self.radius
         print ("The perimeter of this circele is {} units".format(round(perimeter,2)))
+        perimeter = round(perimeter,2)
         return perimeter
     
     def area_ins_reg_hex(self):
         import math
         area_ins_hex =(3*(self.radius**2)*(math.sqrt(3)))/2
         print ("The area of a regular hexagon inscribed in this circele is {} square units".format(round(area_ins_hex,2)))
+        area_ins_hex = round(area_ins_hex,2)
         return area_ins_hex
     
     def area_des_reg_hex(self):
         import math
         area_des_hex =2*(self.radius**2)*(math.sqrt(3))
         print ("The area of a regular hexagon desscribed on this circele is {} square units".format(round(area_des_hex,2)))    
+        area_des_hex = area_des_hex(area,2)
         return area_des_hex
+
+obj1
+
+area(4)
+perimeter(4)
+
+
+
+
