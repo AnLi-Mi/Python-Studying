16badbcadebea80f5989a6df01593e091d087442
AnLi-Mi <a.lipiarz@gmail.com>
2020-09-30 19:07:51 +0200

    connecting to the outside API

---
+++ mini_projects/Music_Artists - API_use/Music_Artists - API_use.py
@@ -0,0 +1,23 @@
+import requests
+
+headers = {'user-agent': 'TestujeAPI-Anna'}
+
+r_api = requests.get('http://my-api-url', headers=headers)
+
+API_KEY = '59df0d91a02d6acff62b031edede3254'
+UDER_AGENT = 'TestujeAPI-Anna'
+
+headers = {
+    'user-agent': USER_AGENT
+}
+
+payload = {
+    'api_key': API_KEY,
+    'method': 'chart.gettopartists',
+    'format': 'json'
+}
+
+r = requests.get('http://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
+print(r.status_code)
+
+
