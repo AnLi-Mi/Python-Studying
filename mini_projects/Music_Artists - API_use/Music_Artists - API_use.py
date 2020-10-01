39e44d5b0e44da4425337fe6b373a08ef9f1af13
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-01 14:30:08 +0200

    function fatching data from given number of result pages

--- mini_projects/Music_Artists - API_use/Music_Artists - API_use.py
+++ mini_projects/Music_Artists - API_use/Music_Artists - API_use.py
@@ -20,7 +20,33 @@ def lastfm_get(payload):
     
 
     response = requests.get(url, headers=headers, params=payload)
-    return response
+    return response.json()
 
-jsonprint(lastfm_get({'method':'chart.gettopartists'}).json())
+#jsonprint(lastfm_get({'method':'chart.gettopartists'}).json())
 
+def loop_through_pages(num_of_pages):
+    page = 1
+    #position =1
+    last_page= num_of_pages
+    #artist_name = []
+
+    while page < last_page +1:
+        result = lastfm_get({'method':'chart.gettopartists', 'page':page})
+        #while position < 51:
+         #   artist = result.json()['artists']['artist'][position]['name']
+         #   artist_name.append(artist)
+       # 
+        jsonprint(result)
+        page+=1
+
+#print (len(artist_name))
+#print('TOP 100 artists:')
+#i=1
+#for artist in artist_name:
+ #   print (f' No {i} - {artist}')
+  #  i+=1
+
+#r=lastfm_get({'method':'chart.gettopartists'})
+#jsonprint(r.json()['artists']['artist'][2]['name'])
+
+loop_through_pages(3)
