a0d58583a69397397159008702149b6a45ea607e
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-01 13:36:30 +0200

    clearing the code - DRY

--- mini_projects/Music_Artists - API_use/Music_Artists - API_use.py
+++ mini_projects/Music_Artists - API_use/Music_Artists - API_use.py
@@ -4,19 +4,6 @@ import json
 API_KEY = '59df0d91a02d6acff62b031edede3254'
 USER_AGENT = 'TestujeAPI-Anna'
 
-headers = {
-    'user-agent': USER_AGENT
-}
-
-payload = {
-    'api_key': API_KEY,
-    'method': 'chart.gettopartists',
-    'format': 'json'
-}
-
-response = requests.get('http://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
-print(response.status_code)
-
 def jsonprint(response):
     response = json.dumps(response, sort_keys=True, indent=4)
     print(response)
@@ -30,9 +17,10 @@ def lastfm_get(payload):
     
     payload['api_key'] = API_KEY
     payload['format'] = 'json'
+    
 
     response = requests.get(url, headers=headers, params=payload)
     return response
 
-jsonprint(lastfm_get(payload).json())
+jsonprint(lastfm_get({'method':'chart.gettopartists'}).json())
 
