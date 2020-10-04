493b5774b703b9b2a285a2bdb500f6a95470bde1
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-04 21:28:33 +0200

    unittest for reat_limit_calls()

--- mini_projects/Music_Artists - API_use/test_LastFM_API.py
+++ mini_projects/Music_Artists - API_use/test_LastFM_API.py
@@ -13,16 +13,19 @@ class MusicAPITest(unittest.TestCase):
 
     
     def test_top_artists(self):
-        #result = 
-        self.assertEqual(LastFM_API.top_artists(1)[0], 'The Weeknd' )
+        result = LastFM_API.top_artists(1)[0]
+        self.assertEqual(result, 'The Weeknd' )
 
     def test_top_tags(self):
-        #result =
-        self.assertEqual(LastFM_API.top_tags(1, 'The Weeknd')[0][1]
-        , 'rnb' )
+        result = LastFM_API.top_tags(1, 'The Weeknd')[0][1]
+        self.assertEqual(result, 'rnb' )
 
-        #result = 
-        self.assertEqual(LastFM_API.top_tags(1, 'Lady Gaga')[0][1], 'pop' )
+        result = LastFM_API.top_tags(1, 'Lady Gaga')[0][1]
+        self.assertEqual(result, 'pop' )
+
+    def test_rate_limit_calling(self):
+        result =LastFM_API.rate_limit_calls()[0]["artists"]["artist"][0]["name"]
+        self.assertEqual(result, 'The Weeknd')
 
 if __name__ == '__main__':
     unittest.main()
