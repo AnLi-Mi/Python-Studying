80da3f8795ff4c88933515999568d6d3aade338b
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-05 15:53:27 +0200

    setting up a new page to test api results on it

--- mini_projects/BMI_calculator_web - Flask_app/app.py
+++ mini_projects/BMI_calculator_web - Flask_app/app.py
@@ -3,6 +3,17 @@ from flask import Flask, render_template, request, json
 
 app=Flask(__name__)
 
+def calc_bmi(w,h):
+    return round(w/(h**2),2)
+
+def rec(bmi):
+    if bmi<18.5:
+        return "You're too skinny"
+    elif bmi>25:
+        return "You need to loose some weight"
+    else:
+        return "You have correct weight"
+
 @app.route('/', methods=['GET', 'POST'])
 def bmi():
     name = ''
@@ -16,21 +27,12 @@ def bmi():
         re = rec(bmi)
     return render_template('bmi.html',  name=name, bmi=bmi, re=re)
 
-def calc_bmi(w,h):
-    return round(w/(h**2),2)
 
-def rec(bmi):
-    if bmi<18.5:
-        return "You're too skinny"
-    elif bmi>25:
-        return "You need to loose some weight"
-    else:
-        return "You have correct weight"
-
-
-@app.route('/test')
+@app.route('/test', methods=['GET','POST'])
 def test():
-    return "testuje!"
+    return render_template('dogspics.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
