3260c0289f568ec172e677278d4fd1a240c9d318
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-06 18:15:16 +0200

    function fatching employees' id and placing them in json dictionary

--- mini_projects/python_rest/server.py
+++ mini_projects/python_rest/server.py
@@ -8,3 +8,16 @@ db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
+
+def get_employees():
+ conn = db_connect.connect() # connecting to the db
+ query = conn.execute("select * from employees") # selecting all employees
+ employees = query.cursor.fetchall() # fetching the results
+ IDs = [e[0] for e in employees] #creating a list of employees' IDs
+ json_ids = {'employees' : IDs}
+ print(json_ids)
+ return json_ids
+ 
+
+get_employees() 
+    
