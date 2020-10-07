b74aa60380b9a4542f6b7fdab02c35f00ad40bca
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-07 14:57:17 +0200

    added object with data of each employee

--- mini_projects/python_rest/app.py
+++ mini_projects/python_rest/app.py
@@ -17,29 +17,31 @@ class Emp(Resource):
         employees = query.cursor.fetchall() # fetching the results
         IDs = [e[0] for e in employees] #creating a list of employees' IDs
         json_ids = [{'employees' : IDs}]
-         
-        #json_ids = json.dumps(json_ids, sort_keys=True, indent=4)
-        
-        return jsonify(json_ids)
+        return json_ids
 
 class Track(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select trackid, name, composer, unitprice from tracks LIMIT 20;")
         dict_list = []
-        for q in query:
-            #print (tuple((q.keys()),q)))
-            pair = dict(zip(q.keys(),q))
-            #print (pair)
+        for q in query:          
+            pair = dict(zip(tuple (q.keys()) ,q))            
             dict_list.append(pair)
         result = [{'data': dict_list}]
         result = jsonify(result)
-        #result = json.dumps(result, sort_keys=True, indent=4)
+        return result
+
+class Emp_Name(Resource):
+    def get(self, emp_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from employees where EmployeeId =%d "  %int(emp_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return result
     
 
 api.add_resource(Emp, '/employees') # Route_1
 api.add_resource(Track, '/tracks') # Route_2
+api.add_resource(Emp_Name, '/employees/<emp_id>') # Route_3
 
 
 if __name__ == '__main__':
