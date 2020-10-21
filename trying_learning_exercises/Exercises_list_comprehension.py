ae9ee0d7a8829f8e0f5c5b2dd4f199b78c9af02c
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-21 13:27:54 +0200

    list comrehension exercise with if clouse

--- trying_learning_exercises/Exercises_list_comprehension.py
+++ trying_learning_exercises/Exercises_list_comprehension.py
@@ -1,4 +1,4 @@
-# creating new list with the dubled elements form the given list
+# Exercise 1 - creating new list with the dubled elements form the given list
 
 
 my_list = [1,2,3,4,5,6,7,8,9,10,11]
@@ -13,8 +13,30 @@ for num in my_list:
 
 print (doubled)
 
-# colution with comprehension
+# solution with comprehension
 
 doubled = [num*2 for num in my_list]
 
 print (doubled)
+
+
+# Exercise 2 - creating a new list only with elements starting with "S" from the given list
+
+my_friends = ["Sara", "Klara", "Sylwia", "Henryk", "Tomek", "Sebastian"]
+name_s =[]
+
+# solution with a for loop and if cluse
+
+for friend in my_friends:
+    if friend.startswith('S'):
+        name_s.append(friend)
+
+print (name_s)
+
+# solution with comprehension
+
+name_s = [friend for friend in my_friends if friend.startswith('S')]
+
+print (name_s)
+
+
