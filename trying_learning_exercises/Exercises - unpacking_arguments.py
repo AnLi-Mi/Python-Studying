8c3f8d60c69583d7f717d78822f965efa6544a64
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-22 18:01:09 +0200

    toying with deconstructing arguments

--- trying_learning_exercises/Exercises - unpacking_arguments.py
+++ trying_learning_exercises/Exercises - unpacking_arguments.py
@@ -1,6 +1,9 @@
+# positional arguments
+
+# Ex 1
 def months_print(*args, year):
     for arg in args:
-        print(f"It's {arg} {year}")
+        print(f"{arg} {year}")
 
 
 
@@ -8,5 +11,30 @@ months = ["jen","feb","mar", "apr", "may", "jun", "jul", "aug", "sept", "oct", "
 
 
 months_print(*months, year='2020')
+
+
+months_print("jen", "feb", "mar", "april", year='is in spring')
+
+# Ex 2
+
+def multiply(*args):
+    total=1
+    for arg in args:
+        total = total*arg
+    return total    
+
+
+print(multiply(1,3,5,7,9))
+
+
+# deconstructing arguments
+
+def calendar (a,b,c,d,e,f,g,h,i,j,k,l, year):
+    print (f'{a}, {c}, {e}, {g}, {h}, {j}, {l}, have 31 days')
+
+
+calendar(*months, year=2020)
+
+
 
 
