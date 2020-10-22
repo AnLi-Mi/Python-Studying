ac3a2e4d9731f9a35672dbed48a69828a66d0334
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-22 18:22:47 +0200

    unpacking variables form a dictionary

--- trying_learning_exercises/Exercises - unpacking_arguments.py
+++ trying_learning_exercises/Exercises - unpacking_arguments.py
@@ -29,12 +29,20 @@ print(multiply(1,3,5,7,9))
 
 # deconstructing arguments
 
-def calendar (a,b,c,d,e,f,g,h,i,j,k,l, year):
+def calendar (a,b,c,d,e,f,g,h,i,j,k,l, year=2020):
     print (f'{a}, {c}, {e}, {g}, {h}, {j}, {l}, have 31 days')
 
 
 calendar(*months, year=2020)
 
+# arguments that are keys or values of a dictionary
 
+def calendar2(a,b,c,d,e,f,g,h,i,j,k,l):
+    print(a,b,c,d,e,f,g,h,i,j,k,l)
+
+months = {"a":31,"b":"28/29","c":31, "d":30, "e":31, "f":30, "g":31, "h":31, "i":30, "j":31, "k":30, "l":31}
+
+calendar2(*months)
+calendar2(**months)
 
 
