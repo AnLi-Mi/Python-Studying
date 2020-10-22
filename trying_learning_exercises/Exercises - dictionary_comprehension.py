1570402539046802f10435ce769cd7a00892fe98
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-22 14:40:01 +0200

    users password verification

--- trying_learning_exercises/Exercises - dictionary_comprehension.py
+++ trying_learning_exercises/Exercises - dictionary_comprehension.py
@@ -25,12 +25,31 @@ users_dictionary2 = {user[1]:user for user in users}
 print(users_dictionary2)
 
 
-# adding the user from the input
+# adding the user from the user's input
 
-user_name = input('Enter your name: ')
-user_password = input('Chooser your name: ')
+username_input = input('Enter your name: ')
+password_input = input('Choose your password: ')
 
-users_dictionary2[user_name] = len(users_dictionary2), user_name, user_password
+users_dictionary[username_input] = len(users_dictionary2), username_input, password_input
 
-print(users_dictionary2)
+print(users_dictionary)
+
+# checking the correctness of an inputed values
+
+username_input2 = input('Enter your name: ')
+
+
+
+if username_input2 in users_dictionary2.keys():
+    _, username, password = users_dictionary2[username_input2]
+    password_input2 = input('Enter your password: ')
+    if password_input2 == password:
+        print ('Access allowed!')
+    else:
+        print ('Wrong password')
+else:
+   print('you ar not registered user!')
+          
+    
+    
 
