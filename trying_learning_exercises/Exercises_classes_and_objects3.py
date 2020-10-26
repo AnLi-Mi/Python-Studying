f08cf5d273e05fc19f5ea3fa94bc4b3648e18ee9
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-26 19:05:05 +0100

    calling instance method

--- trying_learning_exercises/Exercises_classes_and_objects3.py
+++ trying_learning_exercises/Exercises_classes_and_objects3.py
@@ -19,3 +19,20 @@ store1=Store("Store's Name")
 print(store1.name)
 store1.add_item("Apple", 3.5)
 print(store1.stock_price())
+
+#-------------------------------------------------------------------
+
+class ClassTest:
+    def instance_method(self): #it has self as a first parameter so it's na 'instance method'  
+        print (f'This in an instance method of {self}')
+
+
+test1 = ClassTest() #the object is created even without __init__ method
+
+ClassTest.instance_method(test1)
+test1.instance_method()
+
+
+        
+
+         
