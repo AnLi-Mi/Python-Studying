991c708428315b80b42e6baabe2ba2f1f03d6df7
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-27 16:54:21 +0100

    added a class related to the other class

--- trying_learning_exercises/Exercises_classes_and_objects5- composition.py
+++ trying_learning_exercises/Exercises_classes_and_objects5- composition.py
@@ -10,3 +10,24 @@ class JobSite:
 
 website1 = JobSite("BULLDOGJOB", "https://bulldogjob.pl/companies/jobs")
 print (website1)
+
+
+class Job:
+    def __init__(self, title, company, salary, technology, location):
+        self.title = title
+        self.company = company
+        self. salary = salary
+        self.technology = technology
+        self.location = location
+
+    def __str__(self):
+        return f'Position {self.title!r} at {self.company}\n Salary: {self.salary}\n Technology: {self.technology}\n Location: {self.location}'
+
+
+job1 = Job ("Backend Developer", "Intel", 10000, "Python", "Gdańsk")
+job2 = Job ("Frontend Developer", "Pyszne.pl", 8000, "JavaScript", "Warszawa")
+job3 = Job ("Fullstack Developer", "Unit8", 12000, "Ruby, HTML", "Wrocław")
+
+print(job1)
+print(job2)
+print(job3)
