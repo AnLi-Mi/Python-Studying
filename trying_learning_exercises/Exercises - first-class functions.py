5243820978549d39e8ddba9eca89b0bcb71e3e10
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-28 14:31:13 +0100

    implementing global funtion into other function

--- trying_learning_exercises/Exercises - first-class functions.py
+++ trying_learning_exercises/Exercises - first-class functions.py
@@ -1,20 +1,21 @@
+
+def friend_name(friend: dir):
+    return (friend["name"])
+
+
 def search_element(sequence, expected_element):
     for element in sequence:
-        if element == expected_element:
-            return print(element)
+        name = friend_name(element)
+        if name == expected_element:
+            return print(name)
     raise RuntimeError('element not found')
 
 
 friends = [{"name":"Bob", "age":34}, {"name":"Martha", "age":34}, {"name":"Alice", "age":29}, {"name":"Steven", "age":33}, {"name":"Julia", "age":30}, {"name":"Martin", "age":24}]
 
-def friend_name(friend):
-    return print(friend["name"])
+#for friend in friends:
+   # friend_name(friend)
 
-
-for friend in friends:
-    friend_name(friend)
-
-    
 
 try:
     search_element(expected_element = "Julia", sequence = friends)
