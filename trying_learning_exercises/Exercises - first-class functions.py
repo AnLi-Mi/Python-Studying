741ed90fa535bbd5eb15907d921d728bb3cf7c10
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-28 14:08:07 +0100

    toying with the solution when one of variables is not a simple collection

--- trying_learning_exercises/Exercises - first-class functions.py
+++ trying_learning_exercises/Exercises - first-class functions.py
@@ -2,14 +2,18 @@ def search_element(sequence, expected_element):
     for element in sequence:
         if element == expected_element:
             return print(element)
-    #raise RuntimeError('element not found')
+    raise RuntimeError('element not found')
 
 
-friends = ("Bob", "Martha", "Alice", "Steven", "Julia", "Martin")
+friends = [{"name":"Bob", "age":34}, {"name":"Martha", "age":34}, {"name":"Alice", "age":29}, {"name":"Steven", "age":33}, {"name":"Julia", "age":30}, {"name":"Martin", "age":24}]
 
+friends_names = []
+for friend in friends:
+    friends_names.append(friend["name"])
+    
 
 try:
-    search_element(expected_element = "Julia", sequence = friends)
+    search_element(expected_element = "Julia", sequence = friends_names)
 except RuntimeError:
     print ('element not found')
 
