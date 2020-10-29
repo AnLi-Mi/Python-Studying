d25a3c433e8ae555fc0dc727b0cabd896028ed00
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-29 14:19:35 +0100

    decorating the function with @ syntax

--- trying_learning_exercises/Exercises - decorations.py
+++ trying_learning_exercises/Exercises - decorations.py
@@ -1,5 +1,5 @@
-def get_admin_password():
-    return "1234"
+user = {"username": "jose", "access_level": "user"}
+
 
 def make_secure(function):
     def secure_get_password():
@@ -9,9 +9,13 @@ def make_secure(function):
             return f'No admin permissions for user {user["username"]}'
     return secure_get_password
 
+@make_secure  # -> it's equal to "get_admin_password = make_secure(get_admin_password)"
+def get_admin_password():
+    return "1234" 
 
-user = {"username": "jose", "access_level": "user"}
 
+#get_admin_password = make_secure(get_admin_password)
+print(get_admin_password())
 
 
     
@@ -21,22 +25,21 @@ user = {"username": "jose", "access_level": "user"}
 #print(get_admin_password())
 #print(type(get_admin_password()))
 
+#get_admin_password = make_secure(get_admin_password)
 
-get_admin_password = make_secure(get_admin_password)
+#print(get_admin_password)
+#print(type(get_admin_password))
 
-print(get_admin_password)
-print(type(get_admin_password))
+#print(make_secure(get_admin_password))
+#print(type(make_secure(get_admin_password)))
 
-print(make_secure(get_admin_password))
-print(type(make_secure(get_admin_password)))
+#print(make_secure(get_admin_password()))
+#print(type(make_secure(get_admin_password())))
 
-print(make_secure(get_admin_password()))
-print(type(make_secure(get_admin_password())))
+#print(make_secure(get_admin_password)())
+#print(type(make_secure(get_admin_password)()))
 
-print(make_secure(get_admin_password)())
-print(type(make_secure(get_admin_password)()))
-
-print(get_admin_password())
-print(type(get_admin_password()))
+#print(get_admin_password())
+#print(type(get_admin_password()))
 
 
