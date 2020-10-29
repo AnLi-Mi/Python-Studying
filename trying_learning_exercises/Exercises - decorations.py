1bcd593b4b2d629f9925774b9c624ead6fa82534
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-29 14:40:33 +0100

    added parameters to the decorated function

--- trying_learning_exercises/Exercises - decorations.py
+++ trying_learning_exercises/Exercises - decorations.py
@@ -2,22 +2,24 @@ import functools
 
 user = {"username": "jose", "access_level": "user"}
 
-
 def make_secure(function):
     @functools.wraps(function) # -> decorator that keeps the original names of decorated functions - use it on each decorator you write (on the  inner function)!
-    def secure_get_password():
+    def secure_get_password(ars, **kwargs):
         if user["access_level"]=="admin":
-            return function()
+            return function(*ars, **kwargs)
         else:
             return f'No admin permissions for user {user["username"]}'
     return secure_get_password
 
 @make_secure  # -> this is a decorator (not it's inner function). it's equal to "get_admin_password = make_secure(get_admin_password)"
-def get_admin_password():
-    return "1234" 
+def get_password(panel):
+    if panel=="admin":
+        return "1234"
+    elif panel == "billing":
+        return "gfFsvciW!!7r6?2864"
 
-print(get_admin_password())
-print(get_admin_password.__name__)
+print(get_password("admin"))
+print(get_password.__name__)
 
     
 #print(get_admin_password)
