a0af1b0031d0130281031672db44fd21acd9fd85
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-29 16:56:49 +0100

    adding parameters to thr decorators

--- trying_learning_exercises/Exercises - decorations.py
+++ trying_learning_exercises/Exercises - decorations.py
@@ -1,15 +1,20 @@
 import functools
 
-user = {"username": "jose", "access_level": "user"}
+user = {"username": "jose", "access_level": "guest"}
 
-def make_secure(function):
-    @functools.wraps(function) # -> decorator that keeps the original names of decorated functions - use it on each decorator you write (on the  inner function)!
-    def secure_get_password(ars, **kwargs):
-        if user["access_level"]=="admin":
-            return function(*ars, **kwargs)
-        else:
-            return f'No admin permissions for user {user["username"]}'
-    return secure_get_password
+
+
+def make_secure(access_level):
+    def decorator(function):
+        @functools.wraps(function) # -> decorator that keeps the original names of decorated functions - use it on each decorator you write (on the  inner function)!
+        def secure_get_password(*ars, **kwargs):
+            if user["access_level"]==access_level:
+                return function(*ars, **kwargs)
+            else:
+                return f'No {access_level} permissions for user {user["username"]}'
+        return secure_get_password
+    return decorator
+
 
 @make_secure  # -> this is a decorator (not it's inner function). it's equal to "get_admin_password = make_secure(get_admin_password)"
 def get_password(panel):
@@ -18,8 +23,29 @@ def get_password(panel):
     elif panel == "billing":
         return "gfFsvciW!!7r6?2864"
 
-print(get_password("admin"))
-print(get_password.__name__)
+#print(get_password("admin"))
+#print(get_password.__name__)
+
+@make_secure("admin")
+def get_admin_password():
+    return "admin : 1234"
+
+@make_secure("guest")
+def get_user_password():
+    return "user : somepassword"
+
+print(get_admin_password())
+print(get_user_password())
+
+user = {"username": "anna", "access_level": "admin"}
+
+
+print(get_admin_password())
+print(get_user_password())
+
+
+
+
 
     
 #print(get_admin_password)
