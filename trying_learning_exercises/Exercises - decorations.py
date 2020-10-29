9a503834ba3babc87d05f20e8fee858f21c35d12
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-29 14:12:32 +0100

    placing function in function

--- trying_learning_exercises/Exercises - decorations.py
+++ trying_learning_exercises/Exercises - decorations.py
@@ -1,17 +1,42 @@
-user = {"username": "jose", "access_level": "guest"}
-
 def get_admin_password():
     return "1234"
 
-def secure_get_passoword(function):
-    if user["access_level"]=="admin":
-        return function
+def make_secure(function):
+    def secure_get_password():
+        if user["access_level"]=="admin":
+            return function()
+        else:
+            return f'No admin permissions for user {user["username"]}'
+    return secure_get_password
+
+
+user = {"username": "jose", "access_level": "user"}
+
+
+
     
+#print(get_admin_password)
+#print(type(get_admin_password))
+
+#print(get_admin_password())
+#print(type(get_admin_password()))
+
+
+get_admin_password = make_secure(get_admin_password)
+
+print(get_admin_password)
+print(type(get_admin_password))
+
+print(make_secure(get_admin_password))
+print(type(make_secure(get_admin_password)))
+
+print(make_secure(get_admin_password()))
+print(type(make_secure(get_admin_password())))
+
+print(make_secure(get_admin_password)())
+print(type(make_secure(get_admin_password)()))
 
 print(get_admin_password())
-print (secure_get_passoword(get_admin_password))
-    
-
-
+print(type(get_admin_password()))
 
 
