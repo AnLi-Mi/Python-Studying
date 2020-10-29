d668e998578846d3b22b90b5e934c4652a049088
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-29 14:26:53 +0100

    using functool.wraps

--- trying_learning_exercises/Exercises - decorations.py
+++ trying_learning_exercises/Exercises - decorations.py
@@ -1,7 +1,10 @@
+import functools
+
 user = {"username": "jose", "access_level": "user"}
 
 
 def make_secure(function):
+    @functools.wraps(function) # -> decorator that keeps the original names of decorated functions - use it on each decorator you write (on the  inner function)!
     def secure_get_password():
         if user["access_level"]=="admin":
             return function()
@@ -9,14 +12,12 @@ def make_secure(function):
             return f'No admin permissions for user {user["username"]}'
     return secure_get_password
 
-@make_secure  # -> it's equal to "get_admin_password = make_secure(get_admin_password)"
+@make_secure  # -> this is a decorator (not it's inner function). it's equal to "get_admin_password = make_secure(get_admin_password)"
 def get_admin_password():
     return "1234" 
 
-
-#get_admin_password = make_secure(get_admin_password)
 print(get_admin_password())
-
+print(get_admin_password.__name__)
 
     
 #print(get_admin_password)
