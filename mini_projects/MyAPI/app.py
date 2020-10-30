11cc93cadfdc289cfcad833e3e7cf3a8e0603852
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-30 16:22:51 +0100

    creating applications endpoints

--- mini_projects/MyAPI/app.py
+++ mini_projects/MyAPI/app.py
@@ -2,9 +2,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hi, let's statr the app!"
+stores = [
+    {"name": "My great store",
+     "items": [
+         {"name": "My first Item",
+          "price": 15.00
+          }
+     ]
 
+    }
+
+]
+
+@app.route('/store', methods = 'POST')
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/store')
+def get_stores():
+    pass
+
+@app.route('/store/<string:name>/item', methods = 'POST')
+def create_item_in_store(name, price):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
 
 app.run(port=5000)
