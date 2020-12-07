f58c34f5ccb0b88ad8513a2927e42610c6ee1436
AnLi-Mi <a.lipiarz@gmail.com>
2020-12-07 16:09:21 +0100

    addinf get and post endpoints

--- mini_projects/MyAPI2/code/app.py
+++ mini_projects/MyAPI2/code/app.py
@@ -4,10 +4,28 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item["name"]==name:
+                return item
+
+    def post(self, name):
+        #new_item = get
+        item = {"name": name, "price": 12.00}
+        items.append(item)
+        return item
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+
+api.add_resource(Item, '/item/<string:name>')
+
 
 app.run()
