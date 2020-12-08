63c15228ae27ce05aab673e8fcc7b220b82840ee
AnLi-Mi <a.lipiarz@gmail.com>
2020-12-08 16:31:05 +0100

    using safe_str_comp

--- mini_projects/MyAPI2/code/security.py
+++ mini_projects/MyAPI2/code/security.py
@@ -1,3 +1,4 @@
+from werkzeug.security import safe_str_comp
 from user import User
 
 users = [
@@ -12,7 +13,7 @@ userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
     user = username_mapping.get(username, None) # it's the same as username_mapping[username] but we can add default value in the second argument
-    if user and user["password"]==password: #it's the same as if user in not None
+    if user and safe_str_comp(user.password, password): #it's the same as if user in not None; user.password==password
         return user
 
 def identity(payload): # function specific for JWT library
