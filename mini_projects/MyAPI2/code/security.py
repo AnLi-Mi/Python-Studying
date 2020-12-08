165ced495897b17cb188dd069b5357338f50ec1c
AnLi-Mi <a.lipiarz@gmail.com>
2020-12-08 16:11:47 +0100

    added ifr ntity function

--- mini_projects/MyAPI2/code/security.py
+++ mini_projects/MyAPI2/code/security.py
@@ -21,7 +21,9 @@ userid_mapping = {1 :{
 
 def authenticate(username, password):
     user = username_mapping.get(username, None) # it's the same as username_mapping[username] but we can add default value in the second argument
-
-    if user and user[password]==password: #it's the same as if user in not None
+    if user and user["password"]==password: #it's the same as if user in not None
         return user
- 
+
+def identity(payload): # function specific for JWT library
+    user_id = payload['identity']
+    return userid_mapping.get(user_id, None)
