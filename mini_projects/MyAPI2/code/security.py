1f2ac9e69a5864b39323214a187949a286453783
AnLi-Mi <a.lipiarz@gmail.com>
2020-12-08 16:24:45 +0100

    creating the User class instead of dict

--- mini_projects/MyAPI2/code/security.py
+++ mini_projects/MyAPI2/code/security.py
@@ -1,23 +1,14 @@
+from user import User
+
 users = [
-    {"id": 1,
-    "username": "bob",
-    "password":"asdf"
-    }
+    User(1, "bob", "asdf")
 ]
 
-username_mapping = {"bob":{
-    "id": 1,
-    "username": "bob",
-    "password":"asdf"
-    }
-}
+username_mapping = {u.username: u for u in users}
 
-userid_mapping = {1 :{
-    "id": 1,
-    "username": "bob",
-    "password":"asdf"
-    }
-}
+
+userid_mapping = {u.id: u for u in users}
+
 
 def authenticate(username, password):
     user = username_mapping.get(username, None) # it's the same as username_mapping[username] but we can add default value in the second argument
