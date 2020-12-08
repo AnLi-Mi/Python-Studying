85125ea8e4b9b2d834ae013d6c594969e62dc376
AnLi-Mi <a.lipiarz@gmail.com>
2020-12-08 15:32:14 +0100

    added the error control

--- mini_projects/MyAPI2/code/app.py
+++ mini_projects/MyAPI2/code/app.py
@@ -15,6 +15,9 @@ class Item(Resource):
         return {"item": item}, 200 if item is not None else 404 #shorter version - 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x["name"]==name, items), None) is not None: #if filtered item is not none = already exists
+            return {"message" : f"the item with name {name!r} already exists"}, 400 # the code is ok, the user typed in the bad request
+
         new_item = request.get_json()
         item = {"name": name, "price": new_item["price"]}
         items.append(item)
