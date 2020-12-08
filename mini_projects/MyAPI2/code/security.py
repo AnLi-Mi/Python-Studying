477d2764442fe4a1cd60b50d9cf0e762e614b9c7
AnLi-Mi <a.lipiarz@gmail.com>
2020-12-08 16:06:23 +0100

    added authentication function

--- mini_projects/MyAPI2/code/security.py
+++ mini_projects/MyAPI2/code/security.py
@@ -18,3 +18,10 @@ userid_mapping = {1 :{
     "password":"asdf"
     }
 }
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None) # it's the same as username_mapping[username] but we can add default value in the second argument
+
+    if user and user[password]==password: #it's the same as if user in not None
+        return user
+ 
