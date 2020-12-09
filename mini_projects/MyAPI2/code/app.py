07b294f59e52e0f30af2aa6a9cedc13fd489f1c2
AnLi-Mi <a.lipiarz@gmail.com>
2020-12-09 15:45:37 +0100

    assingning the parser to the whole class

--- mini_projects/MyAPI2/code/app.py
+++ mini_projects/MyAPI2/code/app.py
@@ -12,6 +12,12 @@ jwt = JWT(app, authenticate, identity) # /auth
 items = []
 
 class Item(Resource):
+    parser=reqparse.RequestParser() # parser to polsku to analiza skladniowa, no self so it refers to all class
+    parser.add_argument('price',
+            type = float,
+            required = True,
+            help = 'any massage, here - this field cannot be blank')
+
     @jwt_required()
     def get(self, name):
 
@@ -23,15 +29,14 @@ class Item(Resource):
         if next(filter(lambda x: x["name"]==name, items), None) is not None: #if filtered item is not none = already exists
             return {"message" : f"the item with name {name!r} already exists"}, 400 # the code is ok, the user typed in the bad request
 
-        new_item = request.get_json()
+        new_item = Item.parser.parse_args()
         item = {"name": name, "price": new_item["price"]}
         items.append(item)
         return item, 201
 
     def put(self, name):
-        parser=reqparse.RequestParser() # parser to polsku to analiza skladniowa
-        parser.add_argument('price', type = float, required = True, help = 'any massage, here - this field cannot be blank')
-        data = parser.parse_args() # without paresr it would be request.get_json(), it goes through the arguments and choose the valid ones
+
+        data = Item.parser.parse_args() # without paresr it would be request.get_json(), it goes through the arguments and choose the valid ones
 
         item = next(filter(lambda x: x["name"]==name, items), None)
         if item is None:
